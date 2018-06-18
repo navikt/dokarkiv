5e8ee267b698fba2ee6d3380f59a422a222a6e86
Joakim Bjørnstad <joakim.bjornstad@nav.no>
2018-06-18 13:46:21 +0200

    MMA-169 Konfigurerer opp CXF kun når nais kjører

--- dokumentproduksjoninfo/src/main/java/no/nav/dokarkiv/dokumentproduksjoninfo/DokumentproduksjonInfoConfig.java
+++ dokumentproduksjoninfo/src/main/java/no/nav/dokarkiv/dokumentproduksjoninfo/DokumentproduksjonInfoConfig.java
@@ -5,6 +5,7 @@ import org.apache.cxf.jaxws.EndpointImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 import javax.xml.ws.Endpoint;
 
@@ -16,6 +17,7 @@ import javax.xml.ws.Endpoint;
 public class DokumentproduksjonInfoConfig {
 
 	@Bean
+	@Profile("nais")
 	Endpoint dokumentproduksjonInfoV1(Bus bus, DokumentproduksjonInfoEndpoint dokumentproduksjonInfoEndpoint) {
 		EndpointImpl endpoint = new EndpointImpl(bus, dokumentproduksjonInfoEndpoint);
 		endpoint.publish("/dokumentproduksjoninfo/v1");
