b6c2a227acfa86b219871caf35d2bd067ca40d78
Ugur Alpay Cenar <ugur.alpay.cenar@nav.no>
2018-07-31 09:39:50 +0200

    MMA-335 Endret på tjoark111 mapperen til å bruke userId ved populering av kilde navn

--- arkiverdokumentproduksjon/src/main/java/no/nav/dokarkiv/arkiverdokumentproduksjon/tjoark111/OpprettUtgaaendeJournalpostArkiverDokumenterRequestMapper.java
+++ arkiverdokumentproduksjon/src/main/java/no/nav/dokarkiv/arkiverdokumentproduksjon/tjoark111/OpprettUtgaaendeJournalpostArkiverDokumenterRequestMapper.java
@@ -52,7 +52,7 @@ public class OpprettUtgaaendeJournalpostArkiverDokumenterRequestMapper {
 		addKryssreferanse(domainJournalpost, wsRequest.getKryssreferanse());
 
 		kildeNavnPopulator.populateKildeNavnForEntireJournalStructure(domainJournalpost, RequestContextHolder
-				.currentRequestContext().getComponentId());
+				.currentRequestContext().getUserId());
 
 		return OpprettUtgaaendeJournalpostArkiverDokumentRequestTo.builder()
 				.journalpost(domainJournalpost)
