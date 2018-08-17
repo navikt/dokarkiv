be18588ff58e8694b88ad8d4f92655ffaaba734b
Joakim Bjørnstad <joakim.bjornstad@nav.no>
2018-08-17 10:45:15 +0200

    MMA-366 La til sekundær person policy ressurs beriking av xacmlrequest.

--- behandlejournal/src/main/java/no/nav/dokarkiv/behandlejournal/v3/BehandleJournalV3Pep.java
+++ behandlejournal/src/main/java/no/nav/dokarkiv/behandlejournal/v3/BehandleJournalV3Pep.java
@@ -1,11 +1,15 @@
 package no.nav.dokarkiv.behandlejournal.v3;
 
+import static no.nav.abac.xacml.NavAttributter.RESOURCE_ARKIV_GSAK_SAKSID;
 import static no.nav.abac.xacml.NavAttributter.RESOURCE_ARKIV_PENSJON_SAKSID;
+import static no.nav.abac.xacml.NavAttributter.RESOURCE_FELLES_PERSON_TILKNYTTET_FNR;
 import static no.nav.abac.xacml.NavAttributter.RESOURCE_FELLES_TEMA;
 import static org.apache.commons.lang3.StringUtils.isNotEmpty;
 
 import no.nav.dokarkiv.behandlejournal.v3.tjoark065.JournalfoerNotatHenvendelseRequest;
+import no.nav.dokarkiv.core.domain.codes.BrukerTypeCode;
 import no.nav.dokarkiv.core.domain.codes.FagsystemCode;
+import no.nav.dokarkiv.core.domain.entities.Bruker;
 import no.nav.dokarkiv.core.domain.entities.Journalpost;
 import no.nav.dokarkiv.core.logging.AbacLogger;
 import no.nav.dokarkiv.core.security.abac.AuthorizationException;
@@ -39,6 +43,7 @@ public class BehandleJournalV3Pep {
 		final Journalpost journalpost = request.getJournalpost();
 		XacmlRequest xacmlRequest = abacContext.getRequest();
 
+		enrichPolicySekundaerPerson(journalpost, xacmlRequest);
 		enrichPolicyIngenTilgangTilPensjonssaker(journalpost, xacmlRequest);
 		enrichTema(journalpost, xacmlRequest);
 
@@ -56,11 +61,23 @@ public class BehandleJournalV3Pep {
 		}
 	}
 
+	// https://confluence.adeo.no/pages/viewpage.action?pageId=239343219
+	private void enrichPolicySekundaerPerson(Journalpost journalpost, XacmlRequest xacmlRequest) {
+		if (journalpost.getBrukere() != null && journalpost.getBrukere().size() == 1) {
+			Bruker bruker = journalpost.getBrukere().iterator().next();
+			if(BrukerTypeCode.PERSON == bruker.getBrukerType()) {
+				xacmlRequest.resource(RESOURCE_FELLES_PERSON_TILKNYTTET_FNR, bruker.getBrukerId());
+			}
+		}
+	}
+
 	// https://confluence.adeo.no/display/ABAC/Ingen+tilgang+til+pensjons+saker
 	private void enrichPolicyIngenTilgangTilPensjonssaker(Journalpost journalpost, XacmlRequest xacmlRequest) {
 		if (journalpost.getSaksrelasjon() != null && isNotEmpty(journalpost.getSaksrelasjon().getSakId())) {
 			if (FagsystemCode.PEN.equals(journalpost.getSaksrelasjon().getFagsystem())) {
 				xacmlRequest.resource(RESOURCE_ARKIV_PENSJON_SAKSID, journalpost.getSaksrelasjon().getSakId());
+			} else {
+				xacmlRequest.resource(RESOURCE_ARKIV_GSAK_SAKSID, journalpost.getSaksrelasjon().getSakId());
 			}
 		}
 	}
