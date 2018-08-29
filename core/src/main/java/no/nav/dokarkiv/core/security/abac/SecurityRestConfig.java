93eb265cc78e6f78ed76e7972550b038d77a9ed7
Ugur Alpay Cenar <ugur.alpay.cenar@nav.no>
2018-08-29 13:34:13 +0200

    MMA-473 Fikset feil som gjorde at testen feilet

--- core/src/main/java/no/nav/dokarkiv/core/security/abac/SecurityRestConfig.java
+++ core/src/main/java/no/nav/dokarkiv/core/security/abac/SecurityRestConfig.java
@@ -6,8 +6,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @Configuration
+@EnableWebSecurity
 @Import(value = {FregSecurityOidcAutoConfig.class})
 public class SecurityRestConfig {
 
