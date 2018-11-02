11bfa88656f03c159dee64adf825c66eb53e7c50
Ketill Fenne <ketill.fenne@nav.no>
2018-11-02 12:25:02 +0100

    MMA-1218 - Flywayscript for tabeller og entiteter for disse

---
+++ core/src/main/java/no/nav/dokarkiv/core/domain/codes/BegrensningTypeCti.java
@@ -0,0 +1,54 @@
+package no.nav.dokarkiv.core.domain.codes;
+
+import no.nav.dokarkiv.core.domain.ChangeStamp;
+import no.nav.dokarkiv.core.stelvio.CodesTablePeriodicItem;
+
+import javax.persistence.AttributeOverride;
+import javax.persistence.AttributeOverrides;
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+/**
+ * CodesTableItem representing the T_K_BEGRENSNING_TYPE DB table.
+ * 
+ * @author Ketill Fenne, Visma Consulting
+ */
+@Entity
+@Table(name = "T_K_BEGRENSNING_TYPE")
+@AttributeOverrides( { @AttributeOverride(name = "code", column = @Column(name = "begrensning_type")),
+		@AttributeOverride(name = "decode", column = @Column(name = "begrensning_beskrivelse")),
+		@AttributeOverride(name = "valid", column = @Column(name = "er_gyldig")),
+		@AttributeOverride(name = "fromDate", column = @Column(name = "dato_fom")),
+		@AttributeOverride(name = "toDate", column = @Column(name = "dato_tom")) })
+public class BegrensningTypeCti extends CodesTablePeriodicItem<BegrensningTypeCode, String> {
+
+	/**
+	 * ID used for serialization.
+	 */
+	private static final long serialVersionUID = ***gammelt_fnr***63616145L;
+	/**
+	 * Audit information.
+	 */
+	@Embedded
+	private ChangeStamp changeStamp;
+
+	/**
+	 * Default no-arg constructor. Should only be called by persistence
+	 * provider.
+	 */
+	protected BegrensningTypeCti() {
+	}
+
+	/**
+	 * Getter for the changeStamp property.
+	 * 
+	 * @return the changeStamp
+	 */
+	public ChangeStamp getChangeStamp() {
+		return changeStamp;
+	}
+
+}
---
+++ core/src/main/java/no/nav/dokarkiv/core/domain/entities/Begrensning.java
@@ -0,0 +1,93 @@
+package no.nav.dokarkiv.core.domain.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import no.nav.dokarkiv.core.domain.AbstractPersistentVersionedDomainObjectWithKilde;
+import no.nav.dokarkiv.core.domain.codes.BegrensningTypeCode;
+import no.nav.dokarkiv.core.domain.codes.BrukerTypeCode;
+import no.nav.dokarkiv.core.domain.validator.BrukerValidator;
+import org.hibernate.annotations.Cascade;
+import org.hibernate.annotations.CascadeType;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Parameter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+/**
+ * Domain entity class that represents Begrensning.
+ * 
+ * @author Ketill Fenne, Visma Consulting
+ */
+@Entity
+@Table(name = "T_BEGRENSNING")
+@Builder
+@AllArgsConstructor
+public class Begrensning extends AbstractPersistentVersionedDomainObjectWithKilde {
+
+	/** ID used for serialization. */
+	private static final long serialVersionUID = ***gammelt_fnr***36332183L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "t_begrensning_id_seq")
+	@GenericGenerator(name = "t_begrensning_id_seq", strategy = "org.hibernate.id.enhanced.SequenceStyleGenerator",
+					  parameters = { @Parameter(name = "sequence_name", value = "t_begrensning_id_seq")})
+	@Column(name = "begrensning_id", nullable = false)
+	private Long begrensningId;
+
+	@Enumerated(EnumType.STRING)
+	@Column(name = "begrensning_type", length = 50, nullable = false)
+	private BegrensningTypeCode begrensningType;
+
+	@ManyToOne
+	@JoinColumn(name = "journalpost_id")
+	@Cascade({CascadeType.PERSIST, CascadeType.MERGE, CascadeType.SAVE_UPDATE, CascadeType.DETACH})
+	private Journalpost journalpost;
+
+	@ManyToOne
+	@JoinColumn(name = "dokument_info_id")
+	@Cascade({CascadeType.PERSIST, CascadeType.MERGE, CascadeType.SAVE_UPDATE, CascadeType.DETACH})
+	private DokumentInfo dokumentInfo;
+
+	/**
+	 * Default constructor.
+	 */
+	public Begrensning() {
+	}
+
+	public Long getId() {
+		return begrensningId;
+	}
+
+	public BegrensningTypeCode getBegrensningType() {
+		return begrensningType;
+	}
+
+	public void setBegrensningType(BegrensningTypeCode begrensningType) {
+		this.begrensningType = begrensningType;
+	}
+
+	public Journalpost getJournalpost() {
+		return journalpost;
+	}
+
+	public void setJournalpost(Journalpost journalpost) {
+		this.journalpost = journalpost;
+	}
+
+	public DokumentInfo getDokumentInfo() {
+		return dokumentInfo;
+	}
+
+	public void setDokumentInfo(DokumentInfo dokumentInfo) {
+		this.dokumentInfo = dokumentInfo;
+	}
+}
