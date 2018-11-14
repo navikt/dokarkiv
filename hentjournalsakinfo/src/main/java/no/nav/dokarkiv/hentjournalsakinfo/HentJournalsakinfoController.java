86addc4a89a84a69abe2b7c325990baef3677d86
Sigurd Midttun <sigurd.midttun@nav.no>
2018-11-14 16:00:28 +0100

    MMA-1283 Merget med saf

--- hentjournalsakinfo/src/main/java/no/nav/dokarkiv/hentjournalsakinfo/HentJournalsakinfoController.java
+++ hentjournalsakinfo/src/main/java/no/nav/dokarkiv/hentjournalsakinfo/HentJournalsakinfoController.java
@@ -1,12 +1,16 @@
 package no.nav.dokarkiv.hentjournalsakinfo;
 
 import lombok.extern.slf4j.Slf4j;
+import no.nav.dokarkiv.core.domain.codes.VariantFormatCode;
 import no.nav.dokarkiv.hentjournalsakinfo.dto.TilgangJournalpostDto;
+import no.nav.dokarkiv.hentjournalsakinfo.rjoark900.HentJournalpostBulkRequestTo;
 import no.nav.dokarkiv.hentjournalsakinfo.rjoark900.HentJournalpostBulkResponseTo;
 import no.nav.dokarkiv.hentjournalsakinfo.rjoark900.HentJournalpostBulkService;
 import no.nav.dokarkiv.hentjournalsakinfo.rjoark901.HentTilgangJournalpostService;
+import no.nav.dokarkiv.hentjournalsakinfo.rjoark910.VisningJournalpostBulkRequestTo;
 import no.nav.dokarkiv.hentjournalsakinfo.rjoark910.VisningJournalpostBulkResponseTo;
 import no.nav.dokarkiv.hentjournalsakinfo.rjoark910.VisningJournalpostBulkService;
+import no.nav.dokarkiv.hentjournalsakinfo.tjoarkxyz.HentJournalpostListeRequestTo;
 import no.nav.dokarkiv.hentjournalsakinfo.tjoarkxyz.HentJournalpostListeResponseTo;
 import no.nav.dokarkiv.hentjournalsakinfo.tjoarkxyz.HentJournalpostListeService;
 import org.springframework.transaction.annotation.Transactional;
@@ -36,49 +40,48 @@ public class HentJournalsakinfoController {
 	@Inject
 	public HentJournalsakinfoController(HentJournalpostListeService hentJournalpostListeService,
 										HentJournalpostBulkService hentJournalpostBulkService,
-										VisningJournalpostBulkService visningJournalpostBulkService) {
-	public HentJournalsakinfoController(HentJournalpostListeService hentJournalpostListeService,
-				HentTilgangJournalpostService hentTilgangJournalpostService) {
-			this.hentJournalpostListeService = hentJournalpostListeService;
-			this.hentJournalpostBulkService = hentJournalpostBulkService;
-			this.visningJournalpostBulkService = visningJournalpostBulkService;
-			this.hentTilgangJournalpostService = hentTilgangJournalpostService;
-		}
-
-		@Transactional(readOnly = true)
-		@ResponseBody
-		@PostMapping(value = "/hentjournalposter")
-		public HentJournalpostListeResponseTo hentJournalposter (@RequestBody HentJournalpostListeRequestTo hentJournalpostListeRequestTo){
-			log.info("tjoarkxyz har mottatt forespørsel");
-			return hentJournalpostListeService.hentJournalpostListeByArkivIdAndFagsystem(hentJournalpostListeRequestTo);
-		}
-
-		@Transactional(readOnly = true)
-		@ResponseBody
-		@PostMapping(value = "/hentjournalpostbulk")
-		public HentJournalpostBulkResponseTo hentJournalpostBulk (@RequestBody HentJournalpostBulkRequestTo
-		hentJournalpostBulkRequestTo){
-			log.info("rjoark900 henter journalpostbulk.");
-			return hentJournalpostBulkService.hentJournalpostBulk(hentJournalpostBulkRequestTo);
-		}
-
-		@Transactional(readOnly = true)
-		@ResponseBody
-		@PostMapping(value = "/visningjournalpostbulk")
-		public VisningJournalpostBulkResponseTo visningsJournalpostBulk (@RequestBody VisningJournalpostBulkRequestTo
-		visningJournalpostBulkRequestTo){
-			log.info("rjoark910 henter journalposter.");
-			return visningJournalpostBulkService.visningJournalpostBulk(visningJournalpostBulkRequestTo);
-		}
-
-		@ResponseBody
-		@GetMapping(value = "/henttilgangjournalpost/{journalpostId}/{dokumentInfoId}/{variantFormat}")
-		public TilgangJournalpostDto hentTilgangJournalpost (@PathVariable Long journalpostId,
-				@PathVariable Long dokumentInfoId,
-				@PathVariable VariantFormatCode variantFormat){
-			log.info("rjoark901 har mottatt forespørsel om å hente TilgangJournalpost for journalpost med journalpostId={}, dokumentInfoId={} og variantFormat={}",
-					journalpostId, dokumentInfoId, variantFormat.name());
-			return hentTilgangJournalpostService.hentTilgangJournalpost(journalpostId, dokumentInfoId, variantFormat);
-		}
-
+										VisningJournalpostBulkService visningJournalpostBulkService,
+										HentTilgangJournalpostService hentTilgangJournalpostService) {
+		this.hentJournalpostListeService = hentJournalpostListeService;
+		this.hentJournalpostBulkService = hentJournalpostBulkService;
+		this.visningJournalpostBulkService = visningJournalpostBulkService;
+		this.hentTilgangJournalpostService = hentTilgangJournalpostService;
 	}
+
+	@Transactional(readOnly = true)
+	@ResponseBody
+	@PostMapping(value = "/hentjournalposter")
+	public HentJournalpostListeResponseTo hentJournalposter(@RequestBody HentJournalpostListeRequestTo hentJournalpostListeRequestTo) {
+		log.info("tjoarkxyz har mottatt forespørsel");
+		return hentJournalpostListeService.hentJournalpostListeByArkivIdAndFagsystem(hentJournalpostListeRequestTo);
+	}
+
+	@Transactional(readOnly = true)
+	@ResponseBody
+	@PostMapping(value = "/hentjournalpostbulk")
+	public HentJournalpostBulkResponseTo hentJournalpostBulk(@RequestBody HentJournalpostBulkRequestTo
+																	 hentJournalpostBulkRequestTo) {
+		log.info("rjoark900 henter journalpostbulk.");
+		return hentJournalpostBulkService.hentJournalpostBulk(hentJournalpostBulkRequestTo);
+	}
+
+	@Transactional(readOnly = true)
+	@ResponseBody
+	@PostMapping(value = "/visningjournalpostbulk")
+	public VisningJournalpostBulkResponseTo visningsJournalpostBulk(@RequestBody VisningJournalpostBulkRequestTo
+																			visningJournalpostBulkRequestTo) {
+		log.info("rjoark910 henter journalposter.");
+		return visningJournalpostBulkService.visningJournalpostBulk(visningJournalpostBulkRequestTo);
+	}
+
+	@ResponseBody
+	@GetMapping(value = "/henttilgangjournalpost/{journalpostId}/{dokumentInfoId}/{variantFormat}")
+	public TilgangJournalpostDto hentTilgangJournalpost(@PathVariable Long journalpostId,
+														@PathVariable Long dokumentInfoId,
+														@PathVariable VariantFormatCode variantFormat) {
+		log.info("rjoark901 har mottatt forespørsel om å hente TilgangJournalpost for journalpost med journalpostId={}, dokumentInfoId={} og variantFormat={}",
+				journalpostId, dokumentInfoId, variantFormat.name());
+		return hentTilgangJournalpostService.hentTilgangJournalpost(journalpostId, dokumentInfoId, variantFormat);
+	}
+
+}
