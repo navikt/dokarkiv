83bc84fd9a38f0508bddb6481edffbf7484da186
Ugur Alpay Cenar <ugur.alpay.cenar@nav.no>
2018-11-16 16:31:35 +0100

    MMA-1418 La til DirtiesContext

--- core/src/test/java/no/nav/dokarkiv/core/repository/JoarkRepositoryBegrensetTest.java
+++ core/src/test/java/no/nav/dokarkiv/core/repository/JoarkRepositoryBegrensetTest.java
@@ -24,6 +24,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.transaction.TestTransaction;
@@ -234,6 +235,7 @@ public class JoarkRepositoryBegrensetTest {
     }
 
     @Test
+    @DirtiesContext(methodMode = DirtiesContext.MethodMode.BEFORE_METHOD)
     public void shouldNotfindDokumentinfoIdIdByDokumentinfoTilleggsopplysningerNokkelAndVerdiWhenJournalpostIsBegrenset() {
         Journalpost journalpost = createJournalpost();
 
