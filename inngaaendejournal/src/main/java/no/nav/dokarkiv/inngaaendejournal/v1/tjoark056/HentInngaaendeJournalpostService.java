3b567b837ad8359814326d1b10bb3d508181aea6
Ketill Fenne <ketill.fenne@nav.no>
2018-12-05 16:53:44 +0100

    MMA-1607 Fiks etter tilbakemelding på PR

--- inngaaendejournal/src/main/java/no/nav/dokarkiv/inngaaendejournal/v1/tjoark056/HentInngaaendeJournalpostService.java
+++ inngaaendejournal/src/main/java/no/nav/dokarkiv/inngaaendejournal/v1/tjoark056/HentInngaaendeJournalpostService.java
@@ -1,20 +1,19 @@
 package no.nav.dokarkiv.inngaaendejournal.v1.tjoark056;
 
-import com.google.common.collect.Lists;
 import no.nav.dokarkiv.core.domain.codes.VariantFormatCode;
-import no.nav.dokarkiv.core.domain.entities.FilDetaljer;
 import no.nav.dokarkiv.core.domain.entities.Journalpost;
-import no.nav.dokarkiv.core.domain.entities.JournalpostDokumentInfoRelasjon;
 import no.nav.dokarkiv.core.exceptions.JournalpostIkkeFunnetException;
 import no.nav.dokarkiv.core.exceptions.JournalpostIkkeInngaaendeException;
 import no.nav.dokarkiv.core.exceptions.UgyldigInputException;
 import no.nav.dokarkiv.core.repository.JoarkRepositoryBegrenset;
+import no.nav.dokarkiv.inngaaendejournal.v1.tjoark056.to.DokumentInnholdTo;
+import no.nav.dokarkiv.inngaaendejournal.v1.tjoark056.to.DokumentinformasjonTo;
 import no.nav.dokarkiv.inngaaendejournal.v1.tjoark056.to.InngaaendeJournalpostTo;
 import org.springframework.stereotype.Component;
 
 import javax.inject.Inject;
 import java.util.List;
-import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * @author Joakim Bjørnstad, Jbit AS
@@ -22,11 +21,11 @@ import java.util.Set;
 @Component
 public class HentInngaaendeJournalpostService {
 
-    private final JoarkRepositoryBegrenset repository;
+	private final JoarkRepositoryBegrenset repository;
 	private final InngaaendeJournalpostToMapper mapper;
 
 	@Inject
-    public HentInngaaendeJournalpostService(JoarkRepositoryBegrenset repository, InngaaendeJournalpostToMapper mapper) {
+	public HentInngaaendeJournalpostService(JoarkRepositoryBegrenset repository, InngaaendeJournalpostToMapper mapper) {
 		this.repository = repository;
 		this.mapper = mapper;
 	}
@@ -34,7 +33,7 @@ public class HentInngaaendeJournalpostService {
 	public InngaaendeJournalpostTo hentJournalpost(String journalpostId) {
 		try {
 			return doHentJournalpost(journalpostId);
-		} catch(NumberFormatException e) {
+		} catch (NumberFormatException e) {
 			throw new UgyldigInputException("Tjenesten kan ikke utføres fordi input er ugyldig. journalpostId=" + journalpostId, e);
 		}
 	}
@@ -43,40 +42,33 @@ public class HentInngaaendeJournalpostService {
 		assertJournalpostIdIsNotNull(journalpostId);
 		Journalpost journalpost = repository.findById(Long.parseLong(journalpostId)).orElse(null);
 
-		if(journalpost == null) {
+		if (journalpost == null) {
 			throw new JournalpostIkkeFunnetException("Journalpost ikke funnet. journalpostId=" + journalpostId);
 		}
 
-		if(!journalpost.isInngaende()){
+		if (!journalpost.isInngaende()) {
 			throw new JournalpostIkkeInngaaendeException("Journalpost er ikke av type Inngående. journalpostId=" + journalpostId);
 		}
 
-		filterFildetaljer(journalpost);
+		InngaaendeJournalpostTo inngaaendeJournalpostTo = mapper.map(journalpost);
 
-		return mapper.map(journalpost);
+		return filterFildetaljer(inngaaendeJournalpostTo);
 	}
 
 	public void assertJournalpostIdIsNotNull(String journalpostId) {
-		if(journalpostId == null) {
+		if (journalpostId == null) {
 			throw new UgyldigInputException("Tjenesten kan ikke utføres fordi input er ugyldig. journalpostId=null");
 		}
 	}
 
-
-	private void filterFildetaljer(Journalpost journalpost) {
-		Set<JournalpostDokumentInfoRelasjon> infoRelasjoner = journalpost.getJournalpostDokumentInfoRelasjoner();
-
-		for (JournalpostDokumentInfoRelasjon dokInfoRel : infoRelasjoner) {
-			List<FilDetaljer> fdToRemove = Lists.newArrayList();
-			for (FilDetaljer fd : dokInfoRel.getDokumentInfo().getFildetaljerListe()) {
-				if (VariantFormatCode.SLADDET.equals(fd.getVariantFormat())) {
-					fdToRemove.add(fd);
-				}
-			}
-			for (FilDetaljer fd : fdToRemove) {
-				dokInfoRel.getDokumentInfo().removeFilDetaljer(fd);
-			}
+	private InngaaendeJournalpostTo filterFildetaljer(InngaaendeJournalpostTo journalpost) {
+		List<DokumentInnholdTo> filtererdDokumentInnhold = journalpost.getHoveddokument().getFilteredDokumentInnholdTo();
+		journalpost.getHoveddokument().setDokumentInnhold(filtererdDokumentInnhold);
+		for(DokumentinformasjonTo vedlegg:journalpost.getVedlegg()) {
+			filtererdDokumentInnhold = vedlegg.getFilteredDokumentInnholdTo();
+			vedlegg.setDokumentInnhold(filtererdDokumentInnhold);
 		}
+		return journalpost;
 	}
 
 }
