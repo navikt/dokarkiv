4343d3bf028255c4a2f9d9d7e84d15f6b9c0db85
Bjørnar Hunshamar <bjornar.hunshamar@nav.no>
2019-01-29 14:09:37 +0100

    MMA-2243 Fjernet en test

--- arkiverdokumentmottak/src/test/java/no/nav/dokarkiv/arkiverdokumentmottak/tjoark203/v2/JournalforInngaaendeForsendelseV2ValidatorTest.java
+++ arkiverdokumentmottak/src/test/java/no/nav/dokarkiv/arkiverdokumentmottak/tjoark203/v2/JournalforInngaaendeForsendelseV2ValidatorTest.java
@@ -248,19 +248,6 @@ public class JournalforInngaaendeForsendelseV2ValidatorTest {
 	}
 
 	@Test
-	public void shouldFailOnNullDokumenttypeIdForHoveddokument() throws Exception {
-		expectedException.expect(IllegalArgumentException.class);
-		expectedException.expectMessage("Missing required field in request: DokumentInfo.DokumenttypeId");
-
-		for (JournalpostDokumentInfoRelasjon jdir : journalpost.getJournalpostDokumentInfoRelasjoner()) {
-			if (HOVEDDOKUMENT.equals(jdir.getTilknyttetJournalpostSom())) {
-				jdir.getDokumentInfo().setDokumenttypeId(null);
-			}
-		}
-		validator.validate(journalpost);
-	}
-
-	@Test
 	public void shouldNotFailOnNullDokumenttypeIdForVedlegg() throws Exception {
 		for (JournalpostDokumentInfoRelasjon jdir : journalpost.getJournalpostDokumentInfoRelasjoner()) {
 			if (VEDLEGG.equals(jdir.getTilknyttetJournalpostSom())) {
