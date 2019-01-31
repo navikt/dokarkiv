2ac0d688d7957bc1f44612f3120b215db5b619ac
Bjørnar Hunshamar <bjornar.hunshamar@nav.no>
2019-01-31 09:42:06 +0100

    MMA-2243 Sonarfiks

--- arkiverdokumentmottak/src/main/java/no/nav/dokarkiv/arkiverdokumentmottak/tjoark203/v2/JournalforInngaaendeForsendelseV2Validator.java
+++ arkiverdokumentmottak/src/main/java/no/nav/dokarkiv/arkiverdokumentmottak/tjoark203/v2/JournalforInngaaendeForsendelseV2Validator.java
@@ -1,16 +1,12 @@
 package no.nav.dokarkiv.arkiverdokumentmottak.tjoark203.v2;
 
 import static org.apache.commons.lang.Validate.notNull;
-import static org.springframework.util.Assert.hasLength;
 
-import no.nav.dokarkiv.core.domain.codes.TilknyttetJournalpostSomCode;
 import no.nav.dokarkiv.core.domain.entities.Journalpost;
-import no.nav.dokarkiv.core.domain.entities.JournalpostDokumentInfoRelasjon;
 import no.nav.dokarkiv.core.journalbehandling.MandatoryFieldsVerifier;
 import org.springframework.stereotype.Component;
 
 import javax.inject.Inject;
-import java.util.Set;
 
 /**
  * Validator class for JournalforInngaaendeForsendelseV2 (TJOARK203)
