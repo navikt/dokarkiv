53eb99716205830ae314fb6ee10105e5497c0a6e
Ugur Alpay Cenar <ugur.alpay.cenar@nav.no>
2019-02-06 16:50:57 +0100

    MMA-2377 Flyway endringer

--- core/src/main/resources/db/migration/JoarkDS/V20.5.3__aksjonslogg_ny.sql
+++ core/src/main/resources/db/migration/JoarkDS/V20.5.3__aksjonslogg_ny.sql
@@ -7,38 +7,43 @@ alter table t_aksjonslogg
 alter table t_aksjonslogg
   drop column OPPRETTET_AV;
 
-alter table T_AKSJONSLOGG modify JOURNALPOST_ID null;
-alter table T_AKSJONSLOGG add constraint check_jpid_dokid check ( (JOURNALPOST_ID is null AND DOKUMENT_INFO_ID is not null) or (JOURNALPOST_ID is not null AND DOKUMENT_INFO_ID is null) or (JOURNALPOST_ID is not null AND DOKUMENT_INFO_ID is not null));
+alter table T_AKSJONSLOGG
+  modify JOURNALPOST_ID null;
+
+alter table T_AKSJONSLOGG
+  add constraint check_jpid_dokid check ( (JOURNALPOST_ID is null AND DOKUMENT_INFO_ID is not null) or
+                                          (JOURNALPOST_ID is not null AND DOKUMENT_INFO_ID is null) or
+                                          (JOURNALPOST_ID is not null AND DOKUMENT_INFO_ID is not null));
+
+delete from T_AKSJONSLOGG;
+alter table T_AKSJONSLOGG drop column AKSJON;
+alter table T_AKSJONSLOGG add aksjon varchar2(50) default 'ENDRE_SKJERMING' not null
+  constraint t_r_aksjons_type references T_K_AKSJON_TYPE (AKSJON_TYPE);
 
 create table t_arkiv_element_endring
 (
   arkiv_element_endring_id number(11)    not null,
   tidspunkt                timestamp(6)  not null,
-  arkiv_element            varchar2(200)  null,
+  arkiv_element            varchar2(200) null,
   fra_verdi                varchar2(200) null,
   til_verdi                varchar2(200) null,
-  aksjonslogg_id          number(11)    null
+  aksjonslogg_id           number(11)    null
     constraint constraint_aksjons_logg_id
       references T_AKSJONSLOGG,
   constraint arkiv_element_endring_pk primary key (arkiv_element_endring_id)
 );
 
-delete from T_AKSJONSLOGG;
-delete from T_K_AKSJON_TYPE;
-
 rename T_K_AKSJON_TYPE TO T_K_AKSJONS_TYPE;
 
-INSERT INTO T_K_AKSJONS_TYPE (AKSJON_TYPE, BESKRIVELSE, DATO_OPPRETTET, OPPRETTET_AV)
-VALUES ('ENDRE_SKJERMING', 'Endre skjerming på et arkivelement', sysdate, 'Ugur Alpay Cenar');
+update T_K_AKSJONS_TYPE set BESKRIVELSE='Endre skjerming på et arkivelement' where AKSJON_TYPE='ENDRE_BEGRENSNING';
+update T_K_AKSJONS_TYPE set AKSJON_TYPE='ENDRE_SKJERMING' where AKSJON_TYPE='ENDRE_BEGRENSNING';
 
-INSERT INTO T_K_AKSJONS_TYPE (AKSJON_TYPE, BESKRIVELSE, DATO_OPPRETTET, OPPRETTET_AV)
-VALUES ('KASSASJON', 'Kassering av et dokument', sysdate, 'Ugur Alpay Cenar');
+update T_K_AKSJONS_TYPE set BESKRIVELSE='Kassering av et dokument' where AKSJON_TYPE='KASSASJON';
+update T_K_AKSJONS_TYPE set BESKRIVELSE='Sletting av et arkivelement' where AKSJON_TYPE='SLETT';
+update T_K_AKSJONS_TYPE set BESKRIVELSE='Endre skjerming på et arkivelement' where AKSJON_TYPE='ENDRE_SKJERMING';
+update T_K_AKSJONS_TYPE set BESKRIVELSE='Arkivering av et nytt variantformat og dokumentfil' where AKSJON_TYPE='ARKIVERING';
 
-INSERT INTO T_K_AKSJONS_TYPE (AKSJON_TYPE, BESKRIVELSE, DATO_OPPRETTET, OPPRETTET_AV)
-VALUES ('SLETT', 'Sletting av et arkivelement', sysdate, 'Ugur Alpay Cenar');
-
-INSERT INTO T_K_AKSJONS_TYPE (AKSJON_TYPE, BESKRIVELSE, DATO_OPPRETTET, OPPRETTET_AV)
-VALUES ('ARKIVERING', 'Arkivering av et nytt variantformat og dokumentfil', sysdate, 'Ugur Alpay Cenar');
+update T_AKSJONSLOGG set AKSJON='ENDRE_SKJERMING' where AKSJON='ENDRE_BEGRENSNING';
 
 CREATE INDEX xiarkivelementendring
   ON t_arkiv_element_endring (tidspunkt);
