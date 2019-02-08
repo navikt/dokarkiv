e7727a6a1782ec70982d4733b2aa6ce16503666c
Ketill Fenne <ketill.fenne@nav.no>
2019-02-08 14:46:39 +0100

    MMA-2021 merge fra master_pol

--- app/src/main/java/no/nav/dokarkiv/Application.java
+++ app/src/main/java/no/nav/dokarkiv/Application.java
@@ -3,6 +3,7 @@ package no.nav.dokarkiv;
 import no.nav.dokarkiv.arkiverdokumentmottak.ArkiverDokumentmottakConfig;
 import no.nav.dokarkiv.arkiverdokumentproduksjon.ArkiverDokumentproduksjonConfig;
 import no.nav.dokarkiv.arkiverkorrigertdokument.ArkiverKorrigertDokumentConfig;
+import no.nav.dokarkiv.arkivervariant.ArkiverVariantConfig;
 import no.nav.dokarkiv.behandleinngaaendejournal.v1.BehandleInngaaendeJournalV1Config;
 import no.nav.dokarkiv.behandlejournal.v2.BehandleJournalV2Config;
 import no.nav.dokarkiv.behandlejournal.v3.BehandleJournalV3Config;
@@ -21,6 +22,7 @@ import no.nav.dokarkiv.logiskslettdokument.LogiskSlettDokumentConfig;
 import no.nav.dokarkiv.logisktidligkassasjon.LogiskTidligKassasjonConfig;
 import no.nav.dokarkiv.nais.NaisContract;
 import no.nav.dokarkiv.platform.TomcatConfig;
+import no.nav.dokarkiv.skjermarkivenhet.SkjermArkivenhetConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.annotation.Import;
 
