c95d2f19437649347b595d14960d9c110ce265de
Stian Låte <stian.late@nav.no>
2019-02-14 14:53:05 +0100

    MMA-2451 La på mottak av correlationId for hentjournalsakinfo endepunkter.

--- core/src/main/java/no/nav/dokarkiv/core/MDCConstants.java
+++ core/src/main/java/no/nav/dokarkiv/core/MDCConstants.java
@@ -11,6 +11,8 @@ public class MDCConstants {
 	public static final String MDC_USER_ID = "userId";
 	public static final String MDC_USER_NAME = "userName";
 	public static final String MDC_CONSUMER_ID = "consumerId";
+	public static final String MDC_CORRELATION_ID = "correlationId";
+
 	// bruk av ABAC logging
 	public static final String MDC_HTTP_OPERATION = "httpOperation";
 	public static final String MDC_HTTP_ENDPOINT = "httpEndpoint";
