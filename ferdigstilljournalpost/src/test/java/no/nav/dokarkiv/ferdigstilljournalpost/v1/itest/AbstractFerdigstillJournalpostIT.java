83301983107a33e5610246d40508ed9aaedd9c63
Tveter, Martin <Martin.Skjelsvik.Tveter@nav.no>
2019-02-25 10:06:04 +0100

    MMA-2515: tilrettelagt for itester

---
+++ ferdigstilljournalpost/src/test/java/no/nav/dokarkiv/ferdigstilljournalpost/v1/itest/AbstractFerdigstillJournalpostIT.java
@@ -0,0 +1,34 @@
+package no.nav.dokarkiv.ferdigstilljournalpost.v1.itest;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.post;
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
+
+import no.nav.dokarkiv.core.AbstractRestIT;
+import no.nav.dokarkiv.core.CoreConfig;
+import no.nav.dokarkiv.ferdigstilljournalpost.v1.FerdigstillJournalpostConfig;
+import no.nav.freg.security.test.oidc.tools.TestToolsAutoConfig;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
+		classes = {CoreConfig.class, FerdigstillJournalpostConfig.class, TestToolsAutoConfig.class})
+@ActiveProfiles("itest,wiremock,ldap,oidc")
+@AutoConfigureWireMock(port = 0)
+public abstract class AbstractFerdigstillJournalpostIT extends AbstractRestIT {
+
+	static final String URL_FERDIGSTILLJOURNALPOST = "/rest/ferdigstilljournalpost";
+
+	void abacPermit() {
+		stubFor(post(urlEqualTo("/abac"))
+				.willReturn(aResponse().withStatus(HttpStatus.OK.value())
+						.withHeader(org.apache.http.HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+						.withBodyFile("abac/abac-permit.json")));
+	}
+
+}
