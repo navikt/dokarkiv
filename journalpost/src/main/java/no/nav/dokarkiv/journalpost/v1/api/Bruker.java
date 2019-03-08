55fec2d77589a9c887987e8f9d83ee57511b4df4
Tveter, Martin <Martin.Skjelsvik.Tveter@nav.no>
2019-03-08 10:10:50 +0100

    MMA-2659: Kopiert api-objekter fra 'oppdaterJournalpost'-branch

---
+++ journalpost/src/main/java/no/nav/dokarkiv/journalpost/v1/api/Bruker.java
@@ -0,0 +1,27 @@
+package no.nav.dokarkiv.journalpost.v1.api;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotNull;
+
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Bruker {
+	@NotNull(message = "Bruker mangler brukerIdType")
+	@ApiModelProperty(
+			value = "",
+			required = true)
+	private BrukerIdType brukerIdType;
+
+	@NotNull(message = "Bruker mangler identifikator")
+	@ApiModelProperty(
+			value = "",
+			required = true)
+	private String identifikator;
+}
