edf59e49bc29a835bb01c456b65ff7407fedf3f9
Tveter, Martin <Martin.Skjelsvik.Tveter@nav.no>
2019-03-18 15:41:05 +0100

    MMA-2659: Oppdatert swagger-config for oppdater

--- journalpost/src/main/java/no/nav/dokarkiv/journalpost/v1/swagger/SwaggerOppdaterJournalpost.java
+++ journalpost/src/main/java/no/nav/dokarkiv/journalpost/v1/swagger/SwaggerOppdaterJournalpost.java
@@ -3,6 +3,7 @@ package no.nav.dokarkiv.journalpost.v1.swagger;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
+import io.swagger.annotations.Authorization;
 import org.springframework.core.annotation.AliasFor;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -15,7 +16,7 @@ import java.lang.annotation.Target;
 @Target({ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
-@ApiOperation("Oppdater journalpost")
+@ApiOperation(value = "Oppdater journalpost", authorizations = {@Authorization(value = "apiKey")})
 @ApiResponses(value = {
 		@ApiResponse(code = 200, message = "* OK"),
 		@ApiResponse(code = 400, message = "* Kan ikke oppdatere\n* Feil i aksjonslogg \n* Data kan ikke endres for statusen journalposten er på"),
