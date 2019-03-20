3b34975e2e1432578b2bb6bdc2bcd3eeb78635bd
Tveter, Martin <Martin.Skjelsvik.Tveter@nav.no>
2019-03-20 08:46:50 +0100

    MMA-878: missing @component

--- journalpost/src/main/java/no/nav/dokarkiv/journalpost/v1/rjoark203/KopierJournalpostService.java
+++ journalpost/src/main/java/no/nav/dokarkiv/journalpost/v1/rjoark203/KopierJournalpostService.java
@@ -1,7 +1,9 @@
 package no.nav.dokarkiv.journalpost.v1.rjoark203;
 
 import no.nav.dokarkiv.journalpost.v1.api.KopierJournalpostRequest;
+import org.springframework.stereotype.Component;
 
+@Component
 public class KopierJournalpostService {
 
 	public void execute(KopierJournalpostRequest request) {
