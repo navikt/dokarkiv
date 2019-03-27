c6874db0d3739a06d4efd0a9107f7c3f68d91189
Joakim Bjørnstad <joakim.bjornstad@nav.no>
2019-03-27 12:27:35 +0100

    MMA-2805 Ryddet i itest

--- hentjournalsakinfo/src/test/java/no/nav/dokarkiv/hentjournalsakinfo/rjoark901/Rjoark901IT.java
+++ hentjournalsakinfo/src/test/java/no/nav/dokarkiv/hentjournalsakinfo/rjoark901/Rjoark901IT.java
@@ -6,15 +6,7 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.nullValue;
 import static org.junit.Assert.assertThat;
 
-import no.nav.dokarkiv.core.domain.codes.DokumentStatusCode;
-import no.nav.dokarkiv.core.domain.codes.FagomradeCode;
-import no.nav.dokarkiv.core.domain.codes.FagsystemCode;
-import no.nav.dokarkiv.core.domain.codes.JournalStatusCode;
-import no.nav.dokarkiv.core.domain.codes.JournalpostTypeCode;
-import no.nav.dokarkiv.core.domain.codes.MottaksKanalCode;
-import no.nav.dokarkiv.core.domain.codes.SkjermingTypeCode;
 import no.nav.dokarkiv.core.domain.codes.TilknyttetJournalpostSomCode;
-import no.nav.dokarkiv.core.domain.codes.UtsendingsKanalCode;
 import no.nav.dokarkiv.core.domain.codes.VariantFormatCode;
 import no.nav.dokarkiv.core.domain.entities.Bruker;
 import no.nav.dokarkiv.core.domain.entities.DokumentInfo;
@@ -32,23 +24,6 @@ import java.util.Set;
 public class Rjoark901IT extends AbstractHentjournalsakinfoItest {
 
 	private static final String HENTTILGANGJOURNALPOST_URI = "/hentjournalsakinfo/henttilgangjournalpost/{journalpostId}/{dokumentInfoId}/{variantFormat}";
-	private static final String AVSENDER = "bob";
-	private static final String JOURNALFOERT_AV = "test user journalfoert";
-	private static final String JOURNALFOERENDE_ENHET = "test journalfoerende enhet";
-	private static final JournalpostTypeCode JOURNALPOST_TYPE_CODE = JournalpostTypeCode.U;
-	private static final String INNHOLD = "test innhold";
-	private static final FagomradeCode FAGOMRADE = FagomradeCode.PEN;
-	private static final JournalStatusCode JOURNALSTATUS = JournalStatusCode.FS;
-	private static final MottaksKanalCode MOTTAKSKANAL = MottaksKanalCode.NAV_NO;
-	private static final UtsendingsKanalCode UTSENDINGSKANAL = UtsendingsKanalCode.NAV_NO;
-	private static final SkjermingTypeCode SKJERMINGTYPE = SkjermingTypeCode.POL;
-	private static final String SAKID = "test sakid";
-	private static final FagsystemCode SAKRELASJONFAGSYSTEM = FagsystemCode.AO01;
-	private static final Boolean SAKFEILREGISTRERT = true;
-
-	private static final DokumentStatusCode DOKUMENTSTATUS = DokumentStatusCode.UNDER_REDIGERING;
-	private static final String BREVKODE = "test dokumentinfo brevkode";
-	private static final String TITTEL = "test tittel";
 	private static final String EXPECTED_BRUKER_ID = "***gammelt_fnr***";
 
 	@Test
