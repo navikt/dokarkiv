08d459ae2baa65859ffb4a18477bf4ff63424867
Ugur Alpay Cenar <ugur.alpay.cenar@nav.no>
2019-04-09 13:41:23 +0200

    MMA-2851 Feilfiks i test

--- arkivervariant/src/test/java/no/nav/dokarkiv/arkivervariant/AbstractArkiverVariantIT.java
+++ arkivervariant/src/test/java/no/nav/dokarkiv/arkivervariant/AbstractArkiverVariantIT.java
@@ -4,29 +4,51 @@ import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
 import static com.github.tomakehurst.wiremock.client.WireMock.post;
 import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import no.nav.dokarkiv.core.AbstractRestIT;
 import no.nav.dokarkiv.core.CoreConfig;
+import no.nav.dokarkiv.core.security.ldap.NavUser;
 import no.nav.freg.security.test.oidc.tools.TestToolsAutoConfig;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.ldap.core.ContextSource;
+import org.springframework.ldap.core.LdapTemplate;
 import org.springframework.test.context.ActiveProfiles;
 import wiremock.com.google.common.io.Resources;
 
 import java.io.IOException;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.HashSet;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
-		classes = {CoreConfig.class, ArkiverVariantConfig.class, TestToolsAutoConfig.class})
+		classes = {CoreConfig.class, ArkiverVariantConfig.class, TestToolsAutoConfig.class, AbstractArkiverVariantIT.Config.class})
 @ActiveProfiles("itest,wiremock,ldap,oidc")
 @AutoConfigureWireMock(port = 0)
 public abstract class AbstractArkiverVariantIT extends AbstractRestIT {
 
 	protected static final String URL_ARKIVERVARIANT = "/rest/admin/arkivervariant/";
 
+	public static class Config {
+		@Bean
+		LdapTemplate ldapTemplate(ContextSource contextSource) {
+			LdapTemplate mockLdapTemplate = mock(LdapTemplate.class);
+			when(mockLdapTemplate.findOne(any(), any())).thenReturn(NavUser.builder()
+					.memberOf(new HashSet<>(Arrays.asList("0000-GA-joark-vedlikehold")))
+					.userId("Z990782")
+					.userExistsInLdap(true)
+					.build()
+			);
+			return mockLdapTemplate;
+		}
+	}
 
 	public static String classpathToString(String path) {
 		return resourceUrlToString(Resources.getResource(path));
