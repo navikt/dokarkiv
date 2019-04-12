052631001755ab21818cec494cb3a73b3aaa6493
Bjørnar Hunshamar <bjornar.hunshamar@nav.no>
2019-04-12 14:36:08 +0200

    MMA-2761 Skrevet happypath itester

--- journalpost/src/test/java/no/nav/dokarkiv/journalpost/v1/itest/HandterAvvikIT.java
+++ journalpost/src/test/java/no/nav/dokarkiv/journalpost/v1/itest/HandterAvvikIT.java
@@ -2,6 +2,7 @@ package no.nav.dokarkiv.journalpost.v1.itest;
 
 import static org.junit.Assert.assertEquals;
 
+import no.nav.dokarkiv.core.domain.codes.JournalStatusCode;
 import no.nav.dokarkiv.core.domain.entities.Journalpost;
 import no.nav.dokarkiv.core.util.TestDataGenerator;
 import no.nav.dokarkiv.journalpost.v1.util.AvvikstypeConstants;
@@ -38,9 +39,86 @@ public class HandterAvvikIT extends AbstractJournalpostIT {
         TestTransaction.end();
         TestTransaction.start();
 
-        Journalpost feilregistrertJournalpost = joarkRepository.findById(journalpostId).orElseThrow(RuntimeException::new);
+        Journalpost oppdatertJournalpost = joarkRepository.findById(journalpostId).orElseThrow(RuntimeException::new);
 
-        assertEquals(2, journalpost.getJournalpostDokumentInfoRelasjoner().size());
+        assertEquals(oppdatertJournalpost.getSaksrelasjon().getFeilregistrert(), true);
+    }
 
+    @Test
+    public void happyPathOpphevFeilregistrering() throws IOException {
+        abacPermit();
+
+        Journalpost journalpost = TestDataGenerator.createJournalpostWithHoveddokument();
+        journalpost.getSaksrelasjon().setFeilregistrert(true);
+        Long journalpostId = joarkRepository.save(journalpost).getJournalpostId();
+
+        TestTransaction.flagForCommit();
+        TestTransaction.end();
+        TestTransaction.start();
+
+        HttpEntity requestEntity = new HttpEntity(createHeadersWithServiceUserToken());
+        ResponseEntity<String> response = restTemplate.exchange(URL_JOURNALPOST + journalpostId + HANDTER_AVVIK + AvvikstypeConstants.OPPHEV_FEILREGISTRERING, HttpMethod.PATCH, requestEntity, String.class);
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+
+        TestTransaction.flagForCommit();
+        TestTransaction.end();
+        TestTransaction.start();
+
+        Journalpost oppdatertJournalpost = joarkRepository.findById(journalpostId).orElseThrow(RuntimeException::new);
+
+        assertEquals(oppdatertJournalpost.getSaksrelasjon().getFeilregistrert(), false);
+    }
+
+    @Test
+    public void happyPathUkjentBruker() throws IOException {
+        abacPermit();
+
+        Journalpost journalpost = TestDataGenerator.createJournalpostWithHoveddokument();
+        journalpost.setJournalstatus(JournalStatusCode.U);
+        Long journalpostId = joarkRepository.save(journalpost).getJournalpostId();
+
+        TestTransaction.flagForCommit();
+        TestTransaction.end();
+        TestTransaction.start();
+
+        HttpEntity requestEntity = new HttpEntity(createHeadersWithServiceUserToken());
+        ResponseEntity<String> response = restTemplate.exchange(URL_JOURNALPOST + journalpostId + HANDTER_AVVIK + AvvikstypeConstants.UKJENT_BRUKER, HttpMethod.PATCH, requestEntity, String.class);
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+
+        TestTransaction.flagForCommit();
+        TestTransaction.end();
+        TestTransaction.start();
+
+        Journalpost oppdatertJournalpost = joarkRepository.findById(journalpostId).orElseThrow(RuntimeException::new);
+
+        assertEquals(oppdatertJournalpost.getJournalstatus(), JournalStatusCode.UB);
+    }
+
+    @Test
+    public void happyPathAvbryt() throws IOException {
+        abacPermit();
+
+        Journalpost journalpost = TestDataGenerator.createJournalpostWithHoveddokument();
+        journalpost.setJournalstatus(JournalStatusCode.OD);
+        Long journalpostId = joarkRepository.save(journalpost).getJournalpostId();
+
+        TestTransaction.flagForCommit();
+        TestTransaction.end();
+        TestTransaction.start();
+
+        HttpEntity requestEntity = new HttpEntity(createHeadersWithServiceUserToken());
+        ResponseEntity<String> response = restTemplate.exchange(URL_JOURNALPOST + journalpostId + HANDTER_AVVIK + AvvikstypeConstants.AVBRYT, HttpMethod.PATCH, requestEntity, String.class);
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+
+        TestTransaction.flagForCommit();
+        TestTransaction.end();
+        TestTransaction.start();
+
+        Journalpost oppdatertJournalpost = joarkRepository.findById(journalpostId).orElseThrow(RuntimeException::new);
+
+        assertEquals(oppdatertJournalpost.getJournalstatus(), JournalStatusCode.U);
     }
 }
