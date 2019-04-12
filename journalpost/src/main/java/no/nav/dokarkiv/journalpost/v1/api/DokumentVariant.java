802baa3296974cf64889a999bb167b4f533b1233
Martin Burheim Tingstad <Martin.Burheim.Tingstad@nav.no>
2019-04-12 09:54:50 +0200

    MMA-2777: Gjort endringer foreslått av Sonar

--- journalpost/src/main/java/no/nav/dokarkiv/journalpost/v1/api/DokumentVariant.java
+++ journalpost/src/main/java/no/nav/dokarkiv/journalpost/v1/api/DokumentVariant.java
@@ -17,8 +17,9 @@ public class DokumentVariant {
 	@NotNull(message = "Filtype kan ikke være null")
 	@ApiModelProperty(
 			value = "Filtypen til filen som følger, feks PDF/A, JSON eller XML.",
-			example = "PDF/A",
-			required = true)
+			required = true,
+			example = "PDF/A"
+	)
 	private String filtype;
 
 	@NotNull(message = "Variantformat kan ikke være null")
@@ -26,8 +27,9 @@ public class DokumentVariant {
 			value = "ARKIV brukes for dokumentvarianter i menneskelesbart format (for eksempel PDF/A).  Gosys og nav.no henter arkivvariant og viser denne til bruker.\n" +
 					"ORIGINAL skal brukes for dokumentvariant i maskinlesbart format (for eksempel XML og JSON) som brukes for automatisk saksbehandling\n" +
 					"Alle dokumenter må ha én variant med variantFormat ARKIV.",
-			example = "ARKIV",
-			required = true)
+			required = true,
+			example = "ARKIV"
+	)
 	private String variantformat;
 
 	@ApiModelProperty(
