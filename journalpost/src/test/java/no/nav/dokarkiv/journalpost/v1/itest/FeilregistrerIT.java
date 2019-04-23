2f0084b305a8c8e62be9d6d26b5707b11a732bbf
Bjørnar Hunshamar <bjornar.hunshamar@nav.no>
2019-04-23 12:21:30 +0200

    MMA-2761 Sjekker aksjonslogger i itest

--- journalpost/src/test/java/no/nav/dokarkiv/journalpost/v1/itest/FeilregistrerIT.java
+++ journalpost/src/test/java/no/nav/dokarkiv/journalpost/v1/itest/FeilregistrerIT.java
@@ -2,10 +2,13 @@ package no.nav.dokarkiv.journalpost.v1.itest;
 
 import static org.junit.Assert.assertEquals;
 
+import no.nav.dokarkiv.core.domain.codes.AksjonsTypeCode;
 import no.nav.dokarkiv.core.domain.codes.JournalStatusCode;
+import no.nav.dokarkiv.core.domain.entities.AksjonsLogg;
 import no.nav.dokarkiv.core.domain.entities.Journalpost;
 import no.nav.dokarkiv.core.util.TestDataGenerator;
 import no.nav.dokarkiv.journalpost.v1.util.AvvikstypeConstants;
+import org.apache.commons.collections15.IteratorUtils;
 import org.junit.Test;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
@@ -14,10 +17,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.transaction.TestTransaction;
 
 import java.io.IOException;
+import java.util.List;
 
 public class FeilregistrerIT extends AbstractJournalpostIT {
 
     private static final String FEILREGISTRER = "/feilregistrer/";
+    private static final String HJEMMEL = "ARKL";
 
     @Test
     public void happyPathFeilregistrer() throws IOException {
@@ -42,6 +47,17 @@ public class FeilregistrerIT extends AbstractJournalpostIT {
         Journalpost oppdatertJournalpost = joarkRepository.findById(journalpostId).orElseThrow(RuntimeException::new);
 
         assertEquals(oppdatertJournalpost.getSaksrelasjon().getFeilregistrert(), true);
+
+        List<AksjonsLogg> aksjonsLoggList = IteratorUtils.toList(aksjonsLoggRepository.findAll().iterator());
+
+        assertEquals(1, aksjonsLoggList.size());
+
+        AksjonsLogg aksjonsLogg = aksjonsLoggList.get(0);
+        assertEquals(journalpostId, aksjonsLogg.getJournalpostId());
+        assertEquals(SERVICE_USER_ID, aksjonsLogg.getUtfoertAv());
+        assertEquals(AksjonsTypeCode.FEILREGISTRER, aksjonsLogg.getAksjon());
+        assertEquals(HJEMMEL, aksjonsLogg.getHjemmel());
+        assertEquals(1, aksjonsLogg.getArkivElementEndringer().size());
     }
 
     @Test
@@ -68,6 +84,17 @@ public class FeilregistrerIT extends AbstractJournalpostIT {
         Journalpost oppdatertJournalpost = joarkRepository.findById(journalpostId).orElseThrow(RuntimeException::new);
 
         assertEquals(oppdatertJournalpost.getSaksrelasjon().getFeilregistrert(), false);
+
+        List<AksjonsLogg> aksjonsLoggList = IteratorUtils.toList(aksjonsLoggRepository.findAll().iterator());
+
+        assertEquals(1, aksjonsLoggList.size());
+
+        AksjonsLogg aksjonsLogg = aksjonsLoggList.get(0);
+        assertEquals(journalpostId, aksjonsLogg.getJournalpostId());
+        assertEquals(SERVICE_USER_ID, aksjonsLogg.getUtfoertAv());
+        assertEquals(AksjonsTypeCode.OPPHEV_FEILREGISTRERING, aksjonsLogg.getAksjon());
+        assertEquals(HJEMMEL, aksjonsLogg.getHjemmel());
+        assertEquals(1, aksjonsLogg.getArkivElementEndringer().size());
     }
 
     @Test
@@ -94,6 +121,17 @@ public class FeilregistrerIT extends AbstractJournalpostIT {
         Journalpost oppdatertJournalpost = joarkRepository.findById(journalpostId).orElseThrow(RuntimeException::new);
 
         assertEquals(oppdatertJournalpost.getJournalstatus(), JournalStatusCode.UB);
+
+        List<AksjonsLogg> aksjonsLoggList = IteratorUtils.toList(aksjonsLoggRepository.findAll().iterator());
+
+        assertEquals(1, aksjonsLoggList.size());
+
+        AksjonsLogg aksjonsLogg = aksjonsLoggList.get(0);
+        assertEquals(journalpostId, aksjonsLogg.getJournalpostId());
+        assertEquals(SERVICE_USER_ID, aksjonsLogg.getUtfoertAv());
+        assertEquals(AksjonsTypeCode.UKJENT_BRUKER, aksjonsLogg.getAksjon());
+        assertEquals(HJEMMEL, aksjonsLogg.getHjemmel());
+        assertEquals(1, aksjonsLogg.getArkivElementEndringer().size());
     }
 
     @Test
@@ -120,5 +158,16 @@ public class FeilregistrerIT extends AbstractJournalpostIT {
         Journalpost oppdatertJournalpost = joarkRepository.findById(journalpostId).orElseThrow(RuntimeException::new);
 
         assertEquals(oppdatertJournalpost.getJournalstatus(), JournalStatusCode.U);
+
+        List<AksjonsLogg> aksjonsLoggList = IteratorUtils.toList(aksjonsLoggRepository.findAll().iterator());
+
+        assertEquals(1, aksjonsLoggList.size());
+
+        AksjonsLogg aksjonsLogg = aksjonsLoggList.get(0);
+        assertEquals(journalpostId, aksjonsLogg.getJournalpostId());
+        assertEquals(SERVICE_USER_ID, aksjonsLogg.getUtfoertAv());
+        assertEquals(AksjonsTypeCode.AVBRYT, aksjonsLogg.getAksjon());
+        assertEquals(HJEMMEL, aksjonsLogg.getHjemmel());
+        assertEquals(1, aksjonsLogg.getArkivElementEndringer().size());
     }
 }
