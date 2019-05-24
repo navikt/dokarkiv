e927b530b5f9a92d080045b1e8e478c9d14ff81c
Bjørnar Hunshamar <bjornar.hunshamar@nav.no>
2019-05-24 15:04:06 +0200

    MMA-2721 Oppdatert en import

--- journalpost/src/main/java/no/nav/dokarkiv/journalpost/v1/controllers/FeilregistrerRestController.java
+++ journalpost/src/main/java/no/nav/dokarkiv/journalpost/v1/controllers/FeilregistrerRestController.java
@@ -7,6 +7,7 @@ import static no.nav.dokarkiv.core.MDCConstants.MDC_CONSUMER_ID;
 import static no.nav.dokarkiv.core.MDCConstants.MDC_REQUEST_ID;
 import static no.nav.dokarkiv.core.security.abac.JoarkAbacAttributes.UPDATE_ACTION;
 import static no.nav.dokarkiv.journalpost.v1.util.AvvikstypeConstants.AVBRYT;
+import static no.nav.dokarkiv.journalpost.v1.util.AvvikstypeConstants.FEILREGISTRER_SAKSTILKNYTNING;
 import static no.nav.dokarkiv.journalpost.v1.util.AvvikstypeConstants.OPPHEV_FEILREGISTRERT_SAKSTILKNYTNING;
 import static no.nav.dokarkiv.journalpost.v1.util.AvvikstypeConstants.SETT_UKJENT_BRUKER;
 
