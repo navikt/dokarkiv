33cecfdabe0049ab482bed6b9deb110739c6aa69
Martin Burheim Tingstad <Martin.Burheim.Tingstad@nav.no>
2019-06-04 16:04:32 +0200

    MMA-2721: Fjerner ReturInfo fra datamodellen

--- admin/src/test/java/no/nav/dokarkiv/AbstractAdminIT.java
+++ admin/src/test/java/no/nav/dokarkiv/AbstractAdminIT.java
@@ -171,10 +171,6 @@ public abstract class AbstractAdminIT extends AbstractRestIT {
 				.setParameter("jp", journalpostId)
 				.getResultList()
 				.size(), is(0));
-		assertThat(entityManager.createNativeQuery("select '1' from t_retur_info where journalpost_id= :jp")
-				.setParameter("jp", journalpostId)
-				.getResultList()
-				.size(), is(0));
 		assertThat(journalpostDokumentInfoRelasjonRepository.findAllByJournalpostJournalpostId(journalpostId).size(), is(0));
 	}
 
@@ -242,10 +238,6 @@ public abstract class AbstractAdminIT extends AbstractRestIT {
 				.getResultList()
 				.size(), is(1));
 		assertThat(entityManager.createNativeQuery("select '1' from t_jp_tillegg where journalpost_id= :jp")
-				.setParameter("jp", journalpostId)
-				.getResultList()
-				.size(), is(1));
-		assertThat(entityManager.createNativeQuery("select '1' from t_retur_info where journalpost_id= :jp")
 				.setParameter("jp", journalpostId)
 				.getResultList()
 				.size(), is(1));
