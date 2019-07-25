b6476746138da03fd9f53282e7ee52f804ab582b
Martin Burheim Tingstad <Martin.Burheim.Tingstad@nav.no>
2019-07-25 14:40:43 +0200

    MMA-3239: Lagt til nytt behandlingstema ab0423

--- core/src/main/resources/db/migration/JoarkDS/V22.2.2__nytt_behandlingstema.sql
+++ core/src/main/resources/db/migration/JoarkDS/V22.2.2__nytt_behandlingstema.sql
@@ -1,6 +1,6 @@
 INSERT INTO T_K_BEHANDLINGSTEMA (k_behandlingstema, dekode, dato_fom, dato_tom, er_gyldig, dato_opprettet, opprettet_av,
                                  dato_endret, endret_av)
-SELECT 'ab0423', 'Ekspertbistand', DATE '1900-01-01', NULL, '1', sysdate, 'Marti Burhei Tingsta', sysdate, 'Marti Burhei Tingsta'
+SELECT 'ab0423', 'Ekspertbistand', DATE '1900-01-01', NULL, '1', sysdate, 'M. Burheim Tingstad', sysdate, 'M. Burheim Tingstad'
 FROM dual
 WHERE NOT exists(
   SELECT 1
