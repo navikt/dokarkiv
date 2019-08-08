1d34a197f8e6b52a83879840942a3c408250599f
Martin Burheim Tingstad <Martin.Burheim.Tingstad@nav.no>
2019-08-08 17:19:03 +0100

    MMA-3087: Rullet tilbake til Arkiv (V1)

--- journalpost/src/main/java/no/nav/dokarkiv/journalpost/v1/controllers/FeilregistrerJournalpostRestController.java
+++ journalpost/src/main/java/no/nav/dokarkiv/journalpost/v1/controllers/FeilregistrerJournalpostRestController.java
@@ -6,7 +6,7 @@ import static no.nav.abac.xacml.NavAttributter.RESOURCE_FELLES_RESOURCE_TYPE;
 import static no.nav.abac.xacml.StandardAttributter.ACTION_ID;
 import static no.nav.dokarkiv.core.MDCConstants.MDC_CONSUMER_ID;
 import static no.nav.dokarkiv.core.MDCConstants.MDC_REQUEST_ID;
-import static no.nav.dokarkiv.core.security.abac.JoarkAbacAttributes.ARKIV_V2;
+import static no.nav.dokarkiv.core.security.abac.JoarkAbacAttributes.ARKIV;
 import static no.nav.dokarkiv.core.security.abac.JoarkAbacAttributes.UPDATE_ACTION;
 import static no.nav.dokarkiv.journalpost.v1.util.AvvikstypeConstants.AVBRYT;
 import static no.nav.dokarkiv.journalpost.v1.util.AvvikstypeConstants.FEILREGISTRER_SAKSTILKNYTNING;
@@ -70,7 +70,7 @@ public class FeilregistrerJournalpostRestController {
     @SwaggerFeilregistrerSakstilknytning
     @PatchMapping("/{journalpostId}/feilregistrer/" + FEILREGISTRER_SAKSTILKNYTNING)
     @Abac(resources = {@Abac.Attr(key = RESOURCE_FELLES_RESOURCE_TYPE, value = RESOURCE_ARKIV_DOKUMENT),
-                        @Abac.Attr(key = RESOURCE_FELLES_DOMENE, value = ARKIV_V2)},
+                        @Abac.Attr(key = RESOURCE_FELLES_DOMENE, value = ARKIV)},
             actions = @Abac.Attr(key = ACTION_ID, value = UPDATE_ACTION))
     @RestMetrics(value = "dok_request", extraTags = {"process_code", "feilregistrer"}, percentiles = {0.5, 0.95})
     public ResponseEntity<String> feilregistrerSakstilkytning (
@@ -85,7 +85,7 @@ public class FeilregistrerJournalpostRestController {
     @SwaggerOpphevFeilregistrertSakstilknytning
     @PatchMapping("/{journalpostId}/feilregistrer/" + OPPHEV_FEILREGISTRERT_SAKSTILKNYTNING)
     @Abac(resources = {@Abac.Attr(key = RESOURCE_FELLES_RESOURCE_TYPE, value = RESOURCE_ARKIV_DOKUMENT),
-                        @Abac.Attr(key = RESOURCE_FELLES_DOMENE, value = ARKIV_V2)},
+                        @Abac.Attr(key = RESOURCE_FELLES_DOMENE, value = ARKIV)},
             actions = @Abac.Attr(key = ACTION_ID, value = UPDATE_ACTION))
     @RestMetrics(value = "dok_request", extraTags = {"process_code", "feilregistrer"}, percentiles = {0.5, 0.95})
     public ResponseEntity<String> opphevFeilregistrertSakstilknytning (
@@ -100,7 +100,7 @@ public class FeilregistrerJournalpostRestController {
     @SwaggerSettUkjentBruker
     @PatchMapping("/{journalpostId}/feilregistrer/" + SETT_UKJENT_BRUKER)
     @Abac(resources = {@Abac.Attr(key = RESOURCE_FELLES_RESOURCE_TYPE, value = RESOURCE_ARKIV_DOKUMENT),
-                        @Abac.Attr(key = RESOURCE_FELLES_DOMENE, value = ARKIV_V2)},
+                        @Abac.Attr(key = RESOURCE_FELLES_DOMENE, value = ARKIV)},
             actions = @Abac.Attr(key = ACTION_ID, value = UPDATE_ACTION))
     @RestMetrics(value = "dok_request", extraTags = {"process_code", "feilregistrer"}, percentiles = {0.5, 0.95})
     public ResponseEntity<String> settUkjentBruker (
@@ -115,7 +115,7 @@ public class FeilregistrerJournalpostRestController {
     @SwaggerAvbryt
     @PatchMapping("/{journalpostId}/feilregistrer/" + AVBRYT)
     @Abac(resources = {@Abac.Attr(key = RESOURCE_FELLES_RESOURCE_TYPE, value = RESOURCE_ARKIV_DOKUMENT),
-                        @Abac.Attr(key = RESOURCE_FELLES_DOMENE, value = ARKIV_V2)},
+                        @Abac.Attr(key = RESOURCE_FELLES_DOMENE, value = ARKIV)},
             actions = @Abac.Attr(key = ACTION_ID, value = UPDATE_ACTION))
     @RestMetrics(value = "dok_request", extraTags = {"process_code", "feilregistrer"}, percentiles = {0.5, 0.95})
     public ResponseEntity<String> avbryt (
