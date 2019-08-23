cf0ab8b569c2ec53b06ff144cc54917a507b5386
Erik Braten <erik.braten@nav.no>
2019-08-23 13:53:08 +0200

    MMA-3372 DDL for sak_gr tabell. Brukes av DVH

---
+++ core/src/main/resources/db/migration/JoarkDS/V23.1.0__dvh_opprett_sak_gr_tabell.sql
@@ -0,0 +1,43 @@
+CREATE TABLE sak_gr (
+    sak_gr_id                  NUMBER(38)    NOT NULL,
+    sak_id                     NUMBER(10),
+    aktoerid                   VARCHAR2(40),
+    orgnr                      VARCHAR2(9),
+    tema                       VARCHAR2(40),
+    applikasjon                VARCHAR2(40),
+    fagsaknr                   VARCHAR2(40),
+    opprettet_av               VARCHAR2(40),
+    opprettet_tidspunkt        TIMESTAMP,
+    dato_overfort_grensesnitt  TIMESTAMP     NOT NULL,
+    endring_type               CHAR(1)       NOT NULL,
+
+    CONSTRAINT pk_sak_gr PRIMARY KEY (sak_gr_id)
+);
+
+CREATE SEQUENCE sak_gr_seq;
+
+create TRIGGER sak_d_trg
+AFTER DELETE ON sak
+FOR EACH ROW BEGIN
+    INSERT INTO sak_gr
+    (sak_gr_id, sak_id, tema, applikasjon, fagsaknr, opprettet_av, opprettet_tidspunkt, dato_overfort_grensesnitt, endring_type)
+    VALUES
+    (sak_gr_seq.nextval, :old.id, :old.tema, :old.applikasjon, :old.fagsaknr, :old.opprettet_av, :old.opprettet_tidspunkt, CURRENT_TIMESTAMP, 'D');
+END;
+/
+
+create TRIGGER sak_iu_trg
+AFTER INSERT OR UPDATE ON sak
+FOR EACH ROW
+DECLARE
+    endringstype CHAR(1);
+BEGIN
+    IF updating THEN endringstype := 'U'; END IF;
+    IF inserting THEN endringstype :='I'; END IF;
+
+    INSERT INTO sak_gr
+    (sak_gr_id, sak_id, aktoerid, orgnr, tema, applikasjon, fagsaknr, opprettet_av, opprettet_tidspunkt, dato_overfort_grensesnitt, endring_type)
+    VALUES
+    (sak_gr_seq.nextval, :new.id, :new.aktoerid, :new.orgnr, :new.tema, :new.applikasjon, :new.fagsaknr, :new.opprettet_av, :new.opprettet_tidspunkt, CURRENT_TIMESTAMP, endringstype);
+END;
+/
