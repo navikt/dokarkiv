c5d9ce11f027726e2d18ec2fadb7e57442a8d574
Bjørnar Hunshamar <bjornar.hunshamar@nav.no>
2019-09-18 13:41:01 +0200

    Revert "MMA-3284 Implementert GsakConsumer"
    This reverts commit 51c139a

--- core/src/main/java/no/nav/dokarkiv/core/exceptions/GsakOpprettSakTechnicalException.java
+++
@@ -1,7 +0,0 @@
-package no.nav.dokarkiv.core.exceptions;
-
-public class GsakOpprettSakTechnicalException extends DokarkivTechnicalException {
-	public GsakOpprettSakTechnicalException(String message, Throwable cause) {
-		super(message, cause);
-	}
-}
