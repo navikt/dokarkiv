574905efa3981decba8ec00a3d7778da3773a6de
Heidi Elisabeth Sando <heidi.elisabeth.sando@nav.no>
2019-11-01 13:09:27 +0100

    Bugfixes: Fikset på logging

--- journalpost/src/main/java/no/nav/dokarkiv/journalpost/v1/controllers/ArkiverOgJournalfoerRestController.java
+++ journalpost/src/main/java/no/nav/dokarkiv/journalpost/v1/controllers/ArkiverOgJournalfoerRestController.java
@@ -260,9 +260,9 @@ public class ArkiverOgJournalfoerRestController {
 		validateId(journalpostId, "tilknyttJournalpostId");
 		abacSecurityService.assertAccessToJournalpost(journalpostId);
 		RequestContextUtil.createAndSetUsername(MDC.get(MDC_USER_ID), MDC.get(MDCConstants.MDC_CONSUMER_ID));
-		log.info(String.format("Mottat kall til å fjerne vedlegg med dokumentinfoId=%s som er knyttet til journalpost med journalpostId=%s", request.getDokumentId(), journalpostId));
+		log.info("Fjerne vedlegg med dokumentinfoId={} som er knyttet til journalpost med journalpostId={}", request.getDokumentId(), journalpostId);
 		fjernVedleggTilknyttJournalpost.fjernVedleggTilknyttetJournalpost(journalpostId, request);
-		log.info(String.format("Vedlegg med dokumentinfoId=%s som er knyttet til journalpost med journalpostId=%s er fjernet", request.getDokumentId(), journalpostId));
+		log.info("Vedlegg med dokumentinfoId={} som er knyttet til journalpost med journalpostId={} er fjernet", request.getDokumentId(), journalpostId);
 		return ResponseEntity.ok("Vedlegg som knyttet til journalposten fjernet");
 	}
 
