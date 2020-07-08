ce19887b9f3c086f5bd12dcb3c57007e7450e55b
Joakim Bjørnstad <joakim.bjornstad@nav.no>
2020-07-08 17:43:12 +0200

    MMA-4145 Fjernet SakConfig fra app

--- app/src/main/java/no/nav/dokarkiv/Application.java
+++ app/src/main/java/no/nav/dokarkiv/Application.java
@@ -17,7 +17,6 @@ import no.nav.dokarkiv.journalfoerinngaaende.v1.JournalfoerInngaaendeConfig;
 import no.nav.dokarkiv.journalpost.v1.JournalpostConfig;
 import no.nav.dokarkiv.nais.NaisContract;
 import no.nav.dokarkiv.platform.TomcatConfig;
-import no.nav.dokarkiv.sak.SakConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.annotation.Import;
 
@@ -41,7 +40,6 @@ import org.springframework.context.annotation.Import;
 		ArkiverVariantConfig.class,
 		AdminConfig.class,
 		JournalpostConfig.class,
-		SakConfig.class,
 		NaisContract.class})
 public class Application {
 	public static void main(String[] args) {
