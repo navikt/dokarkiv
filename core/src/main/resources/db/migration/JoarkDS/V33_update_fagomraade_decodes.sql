ced4e845a8943926c45fb882808a92c66ebc5580
Joakim Borgersen <joakim.borgersen@nav.no>
2021-02-08 08:29:15 +0100

    MMA-4931: nye decodes T_K_FAGOMRADE

---
+++ core/src/main/resources/db/migration/JoarkDS/V33_update_fagomraade_decodes.sql
@@ -0,0 +1,36 @@
+update T_K_FAGOMRADE
+set dekode = 'Kompensasjon for selvstendig næringsdrivende/frilansere', dato_endret = timestamp '2021-02-08 12:00:00', endret_av = 'Joakim Borgersen'
+where k_fagomrade='FRI'
+;
+update T_K_FAGOMRADE
+set dekode = 'Helsetjenester og ortopediske hjelpemidler', dato_endret = timestamp '2021-02-08 12:00:00', endret_av = 'Joakim Borgersen'
+where k_fagomrade='HEL'
+;
+update T_K_FAGOMRADE
+set dekode = 'Omsorgspenger, pleiepenger og opplæringspenger', dato_endret = timestamp '2021-02-08 12:00:00', endret_av = 'Joakim Borgersen'
+where k_fagomrade='OMS'
+;
+update T_K_FAGOMRADE
+set dekode = 'Saksomkostninger', dato_endret = timestamp '2021-02-08 12:00:00', endret_av = 'Joakim Borgersen'
+where k_fagomrade='SAK'
+;
+update T_K_FAGOMRADE
+set dekode = 'Regnskap/utbetaling', dato_endret = timestamp '2021-02-08 12:00:00', endret_av = 'Joakim Borgersen'
+where k_fagomrade='STO'
+;
+update T_K_FAGOMRADE
+set dekode = 'Trekkhåndtering', dato_endret = timestamp '2021-02-08 12:00:00', endret_av = 'Joakim Borgersen'
+where k_fagomrade='TRK'
+;
+update T_K_FAGOMRADE
+set dekode = 'Aa-registeret', dato_endret = timestamp '2021-02-08 12:00:00', endret_av = 'Joakim Borgersen'
+where k_fagomrade='AAR'
+;
+update T_K_FAGOMRADE
+set dekode = 'Rekruttering og stilling', dato_endret = timestamp '2021-02-08 12:00:00', endret_av = 'Joakim Borgersen'
+where k_fagomrade='REK'
+;
+update T_K_FAGOMRADE
+set dekode = 'Oppfølging - Arbeidsgiver', dato_endret = timestamp '2021-02-08 12:00:00', endret_av = 'Joakim Borgersen'
+where k_fagomrade='OPA'
+;
