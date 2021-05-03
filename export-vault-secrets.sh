d81d9425861125979da307d26a6008d9b56a6630
heidielisabethsando <heidi.elisabeth.sando@nav.no>
2021-05-03 15:36:17 +0200

    MMA-5340: Lagt til appdynamics i dokarkiv - Lagt til kobling mot Vault

--- export-vault-secrets.sh
+++ export-vault-secrets.sh
@@ -30,3 +30,11 @@ then
     echo "Setting SPRING_LDAP_PASSWORD"
     export  SPRING_LDAP_PASSWORD=$(cat /var/run/secrets/nais.io/ldap/password)
 fi
+echo "Exporting appdynamics environment variables"
+if test -f /var/run/secrets/nais.io/appdynamics/appdynamics.env;
+then
+    export $(cat /var/run/secrets/nais.io/appdynamics/appdynamics.env)
+    echo "Appdynamics environment variables exported"
+else
+    echo "No such file or directory found at /var/run/secrets/nais.io/appdynamics/appdynamics.env"
+fi
