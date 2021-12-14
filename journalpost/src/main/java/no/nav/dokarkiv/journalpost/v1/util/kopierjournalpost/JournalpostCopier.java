0a52df288d0a4a4f804301724b214f575de92e6d
S161464 <hakon.snotun@nav.no>
2021-12-14 14:53:28 +0100

    MMA-5686 Formatting

--- journalpost/src/main/java/no/nav/dokarkiv/journalpost/v1/util/kopierjournalpost/JournalpostCopier.java
+++ journalpost/src/main/java/no/nav/dokarkiv/journalpost/v1/util/kopierjournalpost/JournalpostCopier.java
@@ -18,80 +18,80 @@ import static no.nav.dokarkiv.core.MDCConstants.MDC_USER_ID;
 
 public class JournalpostCopier {
 
-    public Journalpost copy(Journalpost journalpost) {
-        Journalpost kopiertJournalpost = journalpost.toBuilder()
-                .journalpostId(null)
-                .opprettetAvNavn(null)
-                .tilleggsopplysninger(copyTilleggsopplysninger(journalpost.getTilleggsopplysninger()))
-                .saksrelasjon(null)
-                .kanalReferanseId(copyKanalReferanseId(journalpost.getKanalReferanseId()))
-                .build();
+	public Journalpost copy(Journalpost journalpost) {
+		Journalpost kopiertJournalpost = journalpost.toBuilder()
+				.journalpostId(null)
+				.opprettetAvNavn(null)
+				.tilleggsopplysninger(copyTilleggsopplysninger(journalpost.getTilleggsopplysninger()))
+				.saksrelasjon(null)
+				.kanalReferanseId(copyKanalReferanseId(journalpost.getKanalReferanseId()))
+				.build();
 
-        kopiertJournalpost.setJournalDato(Date.from(LocalDateTime.now().atZone(ZoneId.systemDefault()).toInstant()));
-        kopiertJournalpost.setEndretAvNavn(MDC.get(MDC_USER_ID));
-        kopiertJournalpost.setJournalfortAvNavn(MDC.get(MDC_USER_ID));
-        kopiertJournalpost.setEndretKildeNavn(MDC.get(MDC_CONSUMER_ID));
-        kopiertJournalpost.setOpprettetKildeNavn(MDC.get(MDC_CONSUMER_ID));
+		kopiertJournalpost.setJournalDato(Date.from(LocalDateTime.now().atZone(ZoneId.systemDefault()).toInstant()));
+		kopiertJournalpost.setEndretAvNavn(MDC.get(MDC_USER_ID));
+		kopiertJournalpost.setJournalfortAvNavn(MDC.get(MDC_USER_ID));
+		kopiertJournalpost.setEndretKildeNavn(MDC.get(MDC_CONSUMER_ID));
+		kopiertJournalpost.setOpprettetKildeNavn(MDC.get(MDC_CONSUMER_ID));
 
-        kopiertJournalpost.setSaksrelasjon(copySaksrelasjon(kopiertJournalpost, journalpost.getSaksrelasjon()));
+		kopiertJournalpost.setSaksrelasjon(copySaksrelasjon(kopiertJournalpost, journalpost.getSaksrelasjon()));
 
-        journalpost.getJournalpostDokumentInfoRelasjoner().forEach(
-                journalpostDokumentInfoRelasjon ->
-                        kopiertJournalpost.addJournalpostDokumentInfoRelasjon(copyJournalpostDokumentInfoRelasjon(kopiertJournalpost, journalpostDokumentInfoRelasjon))
-        );
+		journalpost.getJournalpostDokumentInfoRelasjoner().forEach(
+				journalpostDokumentInfoRelasjon ->
+						kopiertJournalpost.addJournalpostDokumentInfoRelasjon(copyJournalpostDokumentInfoRelasjon(kopiertJournalpost, journalpostDokumentInfoRelasjon))
+		);
 
-        for (Bruker bruker : journalpost.getBrukere()) {
-            kopiertJournalpost.addBruker(cloneBruker(bruker));
-        }
+		for (Bruker bruker : journalpost.getBrukere()) {
+			kopiertJournalpost.addBruker(cloneBruker(bruker));
+		}
 
-        journalpost.getKryssreferanser().forEach(kopiertJournalpost::addKryssReferanse);
+		journalpost.getKryssreferanser().forEach(kopiertJournalpost::addKryssReferanse);
 
-        return kopiertJournalpost;
-    }
+		return kopiertJournalpost;
+	}
 
-    private String copyKanalReferanseId(String kanalReferanseId) {
-        if(kanalReferanseId!=null){
-            return kanalReferanseId +"_"+ UUID.randomUUID().toString().replace("-","").substring(0,8);
-        }
-        return null;
-    }
+	private String copyKanalReferanseId(String kanalReferanseId) {
+		if (kanalReferanseId != null) {
+			return kanalReferanseId + "_" + UUID.randomUUID().toString().replace("-", "").substring(0, 8);
+		}
+		return null;
+	}
 
-    private Bruker cloneBruker(Bruker originalBruker) {
-        Bruker nyBruker = originalBruker.toBuilder().brukerInfoId(null).build();
-        String consumerId = MDC.get(MDC_CONSUMER_ID);
-        nyBruker.setEndretKildeNavn(consumerId);
-        nyBruker.setOpprettetKildeNavn(consumerId);
-        return nyBruker;
-    }
+	private Bruker cloneBruker(Bruker originalBruker) {
+		Bruker nyBruker = originalBruker.toBuilder().brukerInfoId(null).build();
+		String consumerId = MDC.get(MDC_CONSUMER_ID);
+		nyBruker.setEndretKildeNavn(consumerId);
+		nyBruker.setOpprettetKildeNavn(consumerId);
+		return nyBruker;
+	}
 
-    private Map<String, String> copyTilleggsopplysninger(Map<String, String> tilleggsopplysninger) {
-        HashMap<String, String> kopiertTilleggsopplysninger = new HashMap<>();
-        tilleggsopplysninger.forEach(kopiertTilleggsopplysninger::put);
-        return kopiertTilleggsopplysninger;
-    }
+	private Map<String, String> copyTilleggsopplysninger(Map<String, String> tilleggsopplysninger) {
+		HashMap<String, String> kopiertTilleggsopplysninger = new HashMap<>();
+		tilleggsopplysninger.forEach(kopiertTilleggsopplysninger::put);
+		return kopiertTilleggsopplysninger;
+	}
 
-    private Saksrelasjon copySaksrelasjon(Journalpost kopiertJournalpost, Saksrelasjon saksrelasjon) {
-        Saksrelasjon kopiertSaksrelasjon = Saksrelasjon.builder()
-                .fagsystem(saksrelasjon.getFagsystem())
-                .sakId(saksrelasjon.getSakId())
-                .journalpost(kopiertJournalpost)
-                .endretAvNavn(MDC.get(MDC_USER_ID))
-                .feilregistrert(saksrelasjon.getFeilregistrert())
-                .build();
-        kopiertSaksrelasjon.setOpprettetKildeNavn(MDC.get(MDC_CONSUMER_ID));
-        kopiertSaksrelasjon.setEndretKildeNavn(MDC.get(MDC_CONSUMER_ID));
-        return kopiertSaksrelasjon;
-    }
+	private Saksrelasjon copySaksrelasjon(Journalpost kopiertJournalpost, Saksrelasjon saksrelasjon) {
+		Saksrelasjon kopiertSaksrelasjon = Saksrelasjon.builder()
+				.fagsystem(saksrelasjon.getFagsystem())
+				.sakId(saksrelasjon.getSakId())
+				.journalpost(kopiertJournalpost)
+				.endretAvNavn(MDC.get(MDC_USER_ID))
+				.feilregistrert(saksrelasjon.getFeilregistrert())
+				.build();
+		kopiertSaksrelasjon.setOpprettetKildeNavn(MDC.get(MDC_CONSUMER_ID));
+		kopiertSaksrelasjon.setEndretKildeNavn(MDC.get(MDC_CONSUMER_ID));
+		return kopiertSaksrelasjon;
+	}
 
-    private JournalpostDokumentInfoRelasjon copyJournalpostDokumentInfoRelasjon(Journalpost kopiertJournalpost, JournalpostDokumentInfoRelasjon journalpostDokumentInfoRelasjon) {
-        JournalpostDokumentInfoRelasjon kopiertJournalpostdokumentInfoRelasjon = JournalpostDokumentInfoRelasjon.builder()
-                .journalpost(kopiertJournalpost)
-                .dokumentInfo(journalpostDokumentInfoRelasjon.getDokumentInfo())
-                .tilknyttetJournalpostSom(journalpostDokumentInfoRelasjon.getTilknyttetJournalpostSom())
-                .tilknyttetAvNavn(MDC.get(MDC_CONSUMER_ID))
-                .build();
-        kopiertJournalpostdokumentInfoRelasjon.setEndretKildeNavn(MDC.get(MDC_CONSUMER_ID));
-        kopiertJournalpostdokumentInfoRelasjon.setOpprettetKildeNavn(MDC.get(MDC_CONSUMER_ID));
-        return kopiertJournalpostdokumentInfoRelasjon;
-    }
+	private JournalpostDokumentInfoRelasjon copyJournalpostDokumentInfoRelasjon(Journalpost kopiertJournalpost, JournalpostDokumentInfoRelasjon journalpostDokumentInfoRelasjon) {
+		JournalpostDokumentInfoRelasjon kopiertJournalpostdokumentInfoRelasjon = JournalpostDokumentInfoRelasjon.builder()
+				.journalpost(kopiertJournalpost)
+				.dokumentInfo(journalpostDokumentInfoRelasjon.getDokumentInfo())
+				.tilknyttetJournalpostSom(journalpostDokumentInfoRelasjon.getTilknyttetJournalpostSom())
+				.tilknyttetAvNavn(MDC.get(MDC_CONSUMER_ID))
+				.build();
+		kopiertJournalpostdokumentInfoRelasjon.setEndretKildeNavn(MDC.get(MDC_CONSUMER_ID));
+		kopiertJournalpostdokumentInfoRelasjon.setOpprettetKildeNavn(MDC.get(MDC_CONSUMER_ID));
+		return kopiertJournalpostdokumentInfoRelasjon;
+	}
 }
