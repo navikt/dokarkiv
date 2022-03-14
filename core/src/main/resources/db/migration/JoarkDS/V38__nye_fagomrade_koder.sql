c02d127a80925a10c92b195f5bda4631506dbdbe
Joakim Borgersen <joakim.borgersen@nav.no>
2022-03-14 12:50:07 +0100

    Newline

--- core/src/main/resources/db/migration/JoarkDS/V38__nye_fagomrade_koder.sql
+++ core/src/main/resources/db/migration/JoarkDS/V38__nye_fagomrade_koder.sql
@@ -2,3 +2,4 @@ insert into T_K_FAGOMRADE (k_fagomrade,dekode,dato_fom,dato_tom,er_gyldig,dato_o
     VALUES ('EYB','Barnepensjon',date '1900-01-01',NULL,'1',timestamp '2022-03-14 12:00:00','Joakim Borgersen',timestamp '2022-03-14 12:00:00','Joakim Borgersen');
 insert into T_K_FAGOMRADE (k_fagomrade,dekode,dato_fom,dato_tom,er_gyldig,dato_opprettet,opprettet_av,dato_endret,endret_av)
     VALUES ('EYO','Omstillingsstønad',date '1900-01-01',NULL,'1',timestamp '2022-03-14 12:00:00','Joakim Borgersen',timestamp '2022-03-14 12:00:00','Joakim Borgersen');
+
