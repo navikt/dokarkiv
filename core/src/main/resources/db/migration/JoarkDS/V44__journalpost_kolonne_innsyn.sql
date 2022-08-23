6ad7cbbeb6d56dd7230d1dede19a5246fd82c429
Joakim Bjørnstad <joakim.bjornstad@nav.no>
2022-08-23 13:56:45 +0200

    MMA-6327 La til inline foreign key constraint i stedet for seperate spørringer.

--- core/src/main/resources/db/migration/JoarkDS/V44__journalpost_kolonne_innsyn.sql
+++
@@ -1,2 +0,0 @@
-alter table t_journalpost
-    add k_innsyn VARCHAR2(50);
--- core/src/main/resources/db/migration/JoarkDS/V45__journalpost_k_innsyn_fk.sql
+++
@@ -1,3 +0,0 @@
-alter table t_journalpost
-    add constraint fk_k_innsyn
-        foreign key (k_innsyn) references t_k_innsyn;
