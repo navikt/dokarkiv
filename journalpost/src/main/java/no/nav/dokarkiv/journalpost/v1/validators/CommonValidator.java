879ccecb459023981a63472784cb896d84bb159a
Christian Strandenæs <Christian.Fredrik.Osterby.Strandenes@nav.no>
2022-10-11 14:53:50 +0200

    Fiks null-sjekk i validateBoolean

--- journalpost/src/main/java/no/nav/dokarkiv/journalpost/v1/validators/CommonValidator.java
+++ journalpost/src/main/java/no/nav/dokarkiv/journalpost/v1/validators/CommonValidator.java
@@ -19,9 +19,7 @@ public final class CommonValidator {
 	}
 
 	public static void validateBoolean(Boolean value, String feltnavn) {
-		try {
-			assert value != null;
-		} catch (IllegalArgumentException e) {
+		if (value == null) {
 			throw new InputValideringFeiletException(String.format("%s kan ikke være null eller tom.", feltnavn));
 		}
 	}
