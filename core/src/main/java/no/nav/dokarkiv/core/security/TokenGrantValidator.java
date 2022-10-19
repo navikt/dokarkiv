ca46d939249f1c334eb5d39ef75b87410effc495
Christian Strandenæs <Christian.Fredrik.Osterby.Strandenes@nav.no>
2022-10-19 15:33:29 +0200

    Forbedre resten av feilmeldingene i TokenGrantValidator, litt formatering

--- core/src/main/java/no/nav/dokarkiv/core/security/TokenGrantValidator.java
+++ core/src/main/java/no/nav/dokarkiv/core/security/TokenGrantValidator.java
@@ -1,11 +1,9 @@
 package no.nav.dokarkiv.core.security;
 
-import com.nimbusds.jwt.JWT;
 import com.nimbusds.jwt.JWTClaimsSet;
 import com.nimbusds.jwt.JWTParser;
 import lombok.extern.slf4j.Slf4j;
 import no.nav.dokarkiv.core.exceptions.ConsumerUnauthorizedDokarkivFunctionalException;
-import no.nav.dokarkiv.core.exceptions.InputValideringFeiletException;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
 
@@ -28,18 +26,34 @@ public class TokenGrantValidator {
 			var sub = claimsSet.getSubject();
 
 			if (isBlank(oid)) {
-				throw new ConsumerUnauthorizedDokarkivFunctionalException("Access token på Authorization header mangler oid claim. Dette betyr at vi ikke gjenkjenner token som utstedt av NAV sin Azure tenant. Vennligst forsøk på nytt med On-Behalf-Of flow access token fra NAV sin Azure tenant. Hvis ikke dette fungerer, kontakt oss på #team_dokumentløsninger");
+				throw new ConsumerUnauthorizedDokarkivFunctionalException(
+						"Access token på Authorization header mangler oid claim. " +
+								"Dette betyr at vi ikke gjenkjenner token som utstedt av NAV sin Azure tenant. " +
+								"Vennligst forsøk på nytt med On-Behalf-Of flow access token fra NAV sin Azure tenant. " +
+								"Hvis ikke dette fungerer, kontakt oss på #team_dokumentløsninger");
 			}
 			if (isBlank(sub)) {
-				throw new ConsumerUnauthorizedDokarkivFunctionalException("Access Token mangler Subject claim");
+				throw new ConsumerUnauthorizedDokarkivFunctionalException(
+						"Access token på Authorization header mangler subject claim. " +
+								"Dette betyr at vi ikke kan se hvem token er utstedt til av NAV sin Azure tenant. " +
+								"Vennligst forsøk på nytt med On-Behalf-Of flow access token fra NAV sin Azure tenant. " +
+								"Hvis ikke dette fungerer, kontakt oss på #team_dokumentløsninger");
 			}
 			if (StringUtils.equals(oid, sub)) {
-				throw new ConsumerUnauthorizedDokarkivFunctionalException("Access Token er ikke et On-Behalf-Of token");
+				throw new ConsumerUnauthorizedDokarkivFunctionalException(
+						"Access token på Authorization header er ikke er On-Behalf-Of-token" +
+								"Dette betyr at vi ikke gjenkjenner tokenet som riktig type token utstedt av NAV sin Azure tenant. " +
+								"Vennligst forsøk på nytt med On-Behalf-Of flow access token fra NAV sin Azure tenant. " +
+								"Hvis ikke dette fungerer, kontakt oss på #team_dokumentløsninger");
 			}
 			return claimsSet;
 		} catch (ParseException e) {
 			log.warn("En feil oppsto ved parsing av Access Token. Feilmelding={}", e.getMessage(), e);
-			throw new ConsumerUnauthorizedDokarkivFunctionalException("Access Token er ugyldig");
+			throw new ConsumerUnauthorizedDokarkivFunctionalException(
+					"Kunne ikke parse Access token på Authorization header. " +
+							"Dette betyr at vi ikke gjenkjenner dette token som et gyldig token utstedt av NAV sin Azure tenant. " +
+							"Vennligst forsøk på nytt med On-Behalf-Of flow access token fra NAV sin Azure tenant. " +
+							"Hvis ikke dette fungerer, kontakt oss på #team_dokumentløsninger");
 		}
 	}
 }
