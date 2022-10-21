b8128cbc8cceb30cf709ac79d1bc05d7f2c84832
Christian Strandenæs <Christian.Fredrik.Osterby.Strandenes@nav.no>
2022-10-21 16:58:21 +0200

    add constraint keyword and name for foreign key, primary key

--- core/src/main/resources/db/migration/JoarkDS/V46__ny_tabell_utsendings_info.sql
+++ core/src/main/resources/db/migration/JoarkDS/V46__ny_tabell_utsendings_info.sql
@@ -1,5 +1,7 @@
 CREATE TABLE t_utsendings_info (
-    journalpost_id             NUMBER NOT NULL PRIMARY KEY FOREIGN KEY REFERENCES t_journalpost(journalpost_id),
+    journalpost_id             NUMBER NOT NULL
+    CONSTRAINT t_utsendings_info_pk PRIMARY KEY
+    CONSTRAINT t_utsendings_info_fk FOREIGN KEY REFERENCES t_journalpost(journalpost_id),
     adresselinje1              VARCHAR(200),
     adresselinje2              VARCHAR(200),
     adresselinje3              VARCHAR(200),
