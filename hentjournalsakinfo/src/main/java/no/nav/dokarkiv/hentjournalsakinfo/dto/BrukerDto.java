a7dffc779361da4848bfc418b7361e1bccddf47f
Anders Opskar Voldsund <anders.voldsund@nav.no>
2022-11-01 09:04:29 +0100

    MMA-6416 Litt opprydding i fellesklasser

--- hentjournalsakinfo/src/main/java/no/nav/dokarkiv/hentjournalsakinfo/dto/BrukerDto.java
+++ hentjournalsakinfo/src/main/java/no/nav/dokarkiv/hentjournalsakinfo/dto/BrukerDto.java
@@ -2,9 +2,6 @@ package no.nav.dokarkiv.hentjournalsakinfo.dto;
 
 import lombok.Data;
 
-/**
- * @author Joakim Bjørnstad, Jbit AS
- */
 @Data
 public class BrukerDto {
 	private final String brukerId;
--- hentjournalsakinfo/src/main/java/no/nav/dokarkiv/hentjournalsakinfo/dto/TilleggsopplysningDto.java
+++ hentjournalsakinfo/src/main/java/no/nav/dokarkiv/hentjournalsakinfo/dto/TilleggsopplysningDto.java
@@ -2,11 +2,8 @@ package no.nav.dokarkiv.hentjournalsakinfo.dto;
 
 import lombok.Value;
 
-/**
- * @author Joakim Bjørnstad, Jbit AS
- */
 @Value
 public class TilleggsopplysningDto {
-	private final String nokkel;
-	private final String verdi;
+	String nokkel;
+	String verdi;
 }
