c53a14b1c4842498712647b0532f90360215ee92
Joakim Bjørnstad <joakim.bjornstad@nav.no>
2022-12-27 11:44:22 +0100

    MMA-6563 Ryddet test repos

--- core/src/test/java/no/nav/dokarkiv/core/repository/DokumentInfoTestRepository.java
+++ core/src/test/java/no/nav/dokarkiv/core/repository/DokumentInfoTestRepository.java
@@ -2,12 +2,10 @@ package no.nav.dokarkiv.core.repository;
 
 import no.nav.dokarkiv.core.domain.entities.DokumentInfo;
 
-import java.util.List;
-
 /**
  * Brukes kun til test
  */
 public interface DokumentInfoTestRepository extends HibernateRepository<DokumentInfo>, BaseJpaTestRepository<DokumentInfo, Long> {
-	List<DokumentInfo> findByOriginalJournalpostJournalpostId(Long journalpostId);
+
 }
 
