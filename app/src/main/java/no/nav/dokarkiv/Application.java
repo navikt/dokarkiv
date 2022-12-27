0769f169f5e84a6d48f1859aadeb2bcc6b0a8600
Joakim Bjørnstad <joakim.bjornstad@nav.no>
2022-12-27 13:19:09 +0100

    MMA-6559 Sanert Journal_v3 tjenesten
    * Sanert journal modulen som inneholdt Journal_v3 tjenesten

--- app/src/main/java/no/nav/dokarkiv/Application.java
+++ app/src/main/java/no/nav/dokarkiv/Application.java
@@ -8,7 +8,6 @@ import no.nav.dokarkiv.core.CoreConfig;
 import no.nav.dokarkiv.dokumentproduksjoninfo.DokumentproduksjonInfoConfig;
 import no.nav.dokarkiv.hentdokument.HentDokumentConfig;
 import no.nav.dokarkiv.hentjournalsakinfo.HentJournalsakinfoConfig;
-import no.nav.dokarkiv.journal.v3.JournalV3Config;
 import no.nav.dokarkiv.journalpost.v1.JournalpostConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.annotation.Import;
@@ -18,7 +17,6 @@ import org.springframework.context.annotation.Import;
 		ArkiverDokumentproduksjonConfig.class,
 		BehandleJournalV2Config.class,
 		BehandleJournalV3Config.class,
-		JournalV3Config.class,
 		HentDokumentConfig.class,
 		HentJournalsakinfoConfig.class,
 		ArkiverVariantConfig.class,
--- journal/src/main/java/no/nav/dokarkiv/journal/v3/tjoark058/HentKjerneJournalpostListeService.java
+++
@@ -1,9 +0,0 @@
-package no.nav.dokarkiv.journal.v3.tjoark058;
-
-/**
- * @author Stig Strøm
- */
-public interface HentKjerneJournalpostListeService {	
-	
-	HentKjerneJournalpostListeResponseTo hentKjerneJournalpostListe(HentKjerneJournalpostListeRequestTo requestTo);	
-}
