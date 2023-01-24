007950300e3ab2f8b2403cd6f5c9eac7b363450b
tsigab <tsigab.angosom@gmail.com>
2023-01-24 10:48:59 +0100

    MMA-6450 oppryddet unødvendige logger

--- core/src/main/java/no/nav/dokarkiv/core/consumer/pdl/PdlTokenCache.java
+++ core/src/main/java/no/nav/dokarkiv/core/consumer/pdl/PdlTokenCache.java
@@ -1,7 +1,6 @@
 package no.nav.dokarkiv.core.consumer.pdl;
 
 import com.google.common.hash.Hashing;
-import lombok.extern.slf4j.Slf4j;
 import no.nav.dokarkiv.core.consumer.azure.AzureToken;
 import no.nav.dokarkiv.core.properties.DokarkivProperties;
 import no.nav.security.token.support.core.context.TokenValidationContext;
@@ -19,7 +18,6 @@ import static no.nav.dokarkiv.core.cache.CacheConfig.TOKEN_FROM_REQUEST;
 import static no.nav.dokarkiv.core.security.SporingHandlerInterceptor.ISSUER_AZUREV2;
 import static org.apache.commons.lang3.StringUtils.isBlank;
 
-@Slf4j
 @Component
 public class PdlTokenCache {
 
@@ -47,21 +45,18 @@ public class PdlTokenCache {
 			String cacheKey = Hashing.sha256().hashString(jwtToken.getTokenAsString(), StandardCharsets.UTF_8).toString();
 			String accessTokenFromRequest = getAccessTokenFromRequest(cacheKey, jwtToken);
 			if (!jwtToken.containsClaim(OPTIONAL_CLAIM_SET_IDTYP, OPTIONAL_CLAIM_SET_IDTYP_VALUE)) {
-				log.info("Mottatt kall til å hente On-Behalf-Of Access Token fra Azure");
 				return azureToken.onBehalfOfAccessToken(accessTokenFromRequest, dokarkivProperties.getEndpoints().getPdl().getScope());
 			}
 		}
-		log.info("Mottatt kall til å hente client credential Access Token fra Azure");
 		return azureToken.clientCredentialAccessToken(dokarkivProperties.getEndpoints().getPdl().getScope());
 	}
 
 	private String getAccessTokenFromRequest(String cacheKey, JwtToken jwtToken) {
 		Cache tokenRequestCache = cacheManager.getCache(TOKEN_FROM_REQUEST);
 		if (nonNull(tokenRequestCache)) {
-			log.info("Hentet cached token");
 			return tokenRequestCache.get(cacheKey, String.class);
 		}
-		cacheManager.getCache(TOKEN_FROM_REQUEST).put(cacheKey, jwtToken.getTokenAsString());
+		tokenRequestCache.put(cacheKey, jwtToken.getTokenAsString());
 		return jwtToken.getTokenAsString();
 	}
 
