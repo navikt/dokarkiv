46212293855bddedd077d8f5f8496e79b7e4e892
Joakim Bjørnstad <joakim.bjornstad@nav.no>
2023-01-24 16:01:26 +0100

    MMA-6652 Sanerte alle operasjoner i BehandleJournal_v3 unntatt journalfoerNotat

--- behandlejournal/src/main/java/no/nav/dokarkiv/behandlejournal/v3/DefaultBehandleJournalV3Service.java
+++ behandlejournal/src/main/java/no/nav/dokarkiv/behandlejournal/v3/DefaultBehandleJournalV3Service.java
@@ -1,74 +1,17 @@
 package no.nav.dokarkiv.behandlejournal.v3;
 
-import no.nav.dokarkiv.behandlejournal.v3.tjoark060.ArkiverUstrukturertKrav;
-import no.nav.dokarkiv.behandlejournal.v3.tjoark060.ArkiverUstrukturertKravRequest;
-import no.nav.dokarkiv.behandlejournal.v3.tjoark060.ArkiverUstrukturertKravResponse;
-import no.nav.dokarkiv.behandlejournal.v3.tjoark061.LagreVedleggPaaJournalpost;
-import no.nav.dokarkiv.behandlejournal.v3.tjoark061.LagreVedleggPaaJournalpostRequest;
-import no.nav.dokarkiv.behandlejournal.v3.tjoark061.LagreVedleggPaaJournalpostResponse;
-import no.nav.dokarkiv.behandlejournal.v3.tjoark062.FerdigstillDokumentopplasting;
-import no.nav.dokarkiv.behandlejournal.v3.tjoark062.FerdigstillDokumentopplastingRequest;
-import no.nav.dokarkiv.behandlejournal.v3.tjoark063.JournalfoerInngaaendeHenvendelse;
-import no.nav.dokarkiv.behandlejournal.v3.tjoark063.JournalfoerInngaaendeHenvendelseRequest;
-import no.nav.dokarkiv.behandlejournal.v3.tjoark063.JournalfoerInngaaendeHenvendelseResponse;
-import no.nav.dokarkiv.behandlejournal.v3.tjoark064.JournalfoerUtgaaendeHenvendelse;
-import no.nav.dokarkiv.behandlejournal.v3.tjoark064.JournalfoerUtgaaendeHenvendelseRequest;
-import no.nav.dokarkiv.behandlejournal.v3.tjoark064.JournalfoerUtgaaendeHenvendelseResponse;
 import no.nav.dokarkiv.behandlejournal.v3.tjoark065.JournalfoerNotatHenvendelse;
 import no.nav.dokarkiv.behandlejournal.v3.tjoark065.JournalfoerNotatHenvendelseRequest;
 import no.nav.dokarkiv.behandlejournal.v3.tjoark065.JournalfoerNotatHenvendelseResponse;
-import no.nav.dokarkiv.core.exceptions.NoJournalpostFoundException;
 import org.springframework.stereotype.Component;
 
 @Component
 public class DefaultBehandleJournalV3Service implements BehandleJournalV3ServiceBi {
 
-	private final ArkiverUstrukturertKrav arkiverUstrukturertKrav;
-	private final LagreVedleggPaaJournalpost lagreVedleggPaaJournalpost;
-	private final JournalfoerInngaaendeHenvendelse journalfoerInngaaendeHenvendelse;
-	private final FerdigstillDokumentopplasting ferdigstillDokumentopplasting;
-	private final JournalfoerUtgaaendeHenvendelse journalfoerUtgaaendeHenvendelse;
 	private final JournalfoerNotatHenvendelse journalfoerNotatHenvendelse;
 
-	public DefaultBehandleJournalV3Service(ArkiverUstrukturertKrav arkiverUstrukturertKrav, LagreVedleggPaaJournalpost lagreVedleggPaaJournalpost, JournalfoerInngaaendeHenvendelse journalfoerInngaaendeHenvendelse, FerdigstillDokumentopplasting ferdigstillDokumentopplasting, JournalfoerUtgaaendeHenvendelse journalfoerUtgaaendeHenvendelse, JournalfoerNotatHenvendelse journalfoerNotatHenvendelse) {
-		this.arkiverUstrukturertKrav = arkiverUstrukturertKrav;
-		this.lagreVedleggPaaJournalpost = lagreVedleggPaaJournalpost;
-		this.journalfoerInngaaendeHenvendelse = journalfoerInngaaendeHenvendelse;
-		this.ferdigstillDokumentopplasting = ferdigstillDokumentopplasting;
-		this.journalfoerUtgaaendeHenvendelse = journalfoerUtgaaendeHenvendelse;
+	public DefaultBehandleJournalV3Service(JournalfoerNotatHenvendelse journalfoerNotatHenvendelse) {
 		this.journalfoerNotatHenvendelse = journalfoerNotatHenvendelse;
-	}
-
-	@Override
-	public ArkiverUstrukturertKravResponse arkiverUstrukturertKrav(
-			ArkiverUstrukturertKravRequest arkiverUstrukturertKravRequest) {
-		return arkiverUstrukturertKrav.arkiverUstrukturertKrav(arkiverUstrukturertKravRequest);
-	}
-
-	@Override
-	public LagreVedleggPaaJournalpostResponse lagreVedleggPaaJournalpost(
-			LagreVedleggPaaJournalpostRequest lagreVedleggPaaJournalpostRequest) throws NoJournalpostFoundException {
-		return lagreVedleggPaaJournalpost.lagreVedleggPaaJournalpost(lagreVedleggPaaJournalpostRequest);
-	}
-
-	@Override
-	public JournalfoerInngaaendeHenvendelseResponse journalfoerInngaaendeHenvendelse(
-			JournalfoerInngaaendeHenvendelseRequest journalfoerInngaaendeHenvendelseRequest) {
-		return journalfoerInngaaendeHenvendelse
-				.journalfoerInngaaendeHenvendelse(journalfoerInngaaendeHenvendelseRequest);
-	}
-
-	@Override
-	public void ferdigstillDokumentopplasting(FerdigstillDokumentopplastingRequest ferdigstillDokumentOpplastingRequest)
-			throws NoJournalpostFoundException {
-		ferdigstillDokumentopplasting.ferdigstillDokumentOpplasting(ferdigstillDokumentOpplastingRequest);
-	}
-
-	@Override
-	public JournalfoerUtgaaendeHenvendelseResponse journalfoerUtgaaendeHenvendelse(
-			JournalfoerUtgaaendeHenvendelseRequest journalfoerUtgaaendeHenvendelseRequest) {
-		return journalfoerUtgaaendeHenvendelse
-				.journalfoerUtgaaendeHenvendelse(journalfoerUtgaaendeHenvendelseRequest);
 	}
 
 	@Override
