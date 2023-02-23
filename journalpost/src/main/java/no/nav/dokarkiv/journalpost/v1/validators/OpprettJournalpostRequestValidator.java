17e8a3f7e626902d3f493ae95c440a361e2acdeb
Kristian Laskemoen <61700528+L145025@users.noreply.github.com>
2023-02-23 10:49:04 +0100

    glemt formatering innført OpprettJournalpostRequestValidator.java
    Co-authored-by: Joakim Bjørnstad <joakim.bjornstad@nav.no>

--- journalpost/src/main/java/no/nav/dokarkiv/journalpost/v1/validators/OpprettJournalpostRequestValidator.java
+++ journalpost/src/main/java/no/nav/dokarkiv/journalpost/v1/validators/OpprettJournalpostRequestValidator.java
@@ -138,7 +138,7 @@ public class OpprettJournalpostRequestValidator {
 	}
 	private void softValidateDato(Date dato, String datoFeltNavn) {
 		Date systemdato = Date.from(LocalDateTime.now().atZone(ZoneId.systemDefault()).plusSeconds(3).toInstant());
-		if (dato.compareTo(systemdato) > 0){
+		if (dato.compareTo(systemdato) > 0) {
 			log.warn(format("Validering av %s feilet. Dato kan ikke være frem i tid. %s er %s og nå tid er %s", datoFeltNavn, datoFeltNavn, dato, systemdato));
 		}
 	}
