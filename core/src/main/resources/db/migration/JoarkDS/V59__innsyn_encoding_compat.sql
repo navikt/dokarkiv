84c3c2c95638f46b70851aaf1a85ffc9ace89aed
Joakim Bjørnstad <joakim.bjornstad@nav.no>
2023-06-16 15:05:29 +0200

    MMA-6346 La til innsyn verdien SKJULES_BRUKERS_ONSKE (#437)
    * MMA-6346 La til nytt innsyn SKJULES_BRUKERS_ONSKE
    * Erstatter SKJULES_BRUKERS_ØNSKE

---
+++ core/src/main/resources/db/migration/JoarkDS/V59__innsyn_encoding_compat.sql
@@ -0,0 +1,3 @@
+insert into t_k_innsyn (k_innsyn, beskrivelse, dato_opprettet, opprettet_av)
+values ('SKJULES_BRUKERS_ONSKE', 'Skjules på nav.no etter brukers eget ønske',
+        current_timestamp, 'MMA-6327');
