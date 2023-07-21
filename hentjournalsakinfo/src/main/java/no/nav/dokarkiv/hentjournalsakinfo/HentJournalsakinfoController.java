235429d1e381c3e0ca8e38f5ec34c8b5e85da75d
Joakim Bjørnstad <joakim.bjornstad@nav.no>
2023-07-21 13:07:30 +0200

    MMA-6972 Sanert hentjournalsakinfo/hentDokument

--- hentjournalsakinfo/src/main/java/no/nav/dokarkiv/hentjournalsakinfo/HentJournalsakinfoController.java
+++ hentjournalsakinfo/src/main/java/no/nav/dokarkiv/hentjournalsakinfo/HentJournalsakinfoController.java
@@ -3,8 +3,6 @@ package no.nav.dokarkiv.hentjournalsakinfo;
 import lombok.extern.slf4j.Slf4j;
 import no.nav.dokarkiv.core.domain.codes.VariantFormatCode;
 import no.nav.dokarkiv.core.metrics.RestMetrics;
-import no.nav.dokarkiv.core.stelvio.RequestContextUtil;
-import no.nav.dokarkiv.core.util.MimeTypeMapper;
 import no.nav.dokarkiv.hentjournalsakinfo.rjoark900.FinnJournalposterRequestTo;
 import no.nav.dokarkiv.hentjournalsakinfo.rjoark900.FinnJournalposterResponseTo;
 import no.nav.dokarkiv.hentjournalsakinfo.rjoark900.FinnJournalposterService;
@@ -18,12 +16,7 @@ import no.nav.dokarkiv.hentjournalsakinfo.rjoark903.TilknyttedeJournalposterServ
 import no.nav.dokarkiv.hentjournalsakinfo.rjoark904.FinnJournalposterStatusRequestTo;
 import no.nav.dokarkiv.hentjournalsakinfo.rjoark904.FinnJournalposterStatusResponseTo;
 import no.nav.dokarkiv.hentjournalsakinfo.rjoark904.FinnJournalposterStatusService;
-import no.nav.dokarkiv.hentjournalsakinfo.rjoark920.SafHentDokumentResponse;
-import no.nav.dokarkiv.hentjournalsakinfo.rjoark920.SafHentDokumentService;
 import no.nav.security.token.support.core.api.Unprotected;
-import org.slf4j.MDC;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -32,7 +25,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Base64;
 import java.util.List;
 
 @Slf4j
@@ -40,22 +32,17 @@ import java.util.List;
 @RestController
 @RequestMapping("/hentjournalsakinfo")
 public class HentJournalsakinfoController {
-	public static final String RJOARK_920 = "rjoark920";
-	private final SafHentDokumentService safHentDokumentService;
 	private final SafHentJournalpostService safHentJournalpostService;
-	private final MimeTypeMapper mimeTypeMapper = new MimeTypeMapper();
 	private final FinnJournalposterService finnJournalposterService;
 	private final FinnJournalposterStatusService finnJournalposterStatusService;
 	private final HentTilgangJournalpostService hentTilgangJournalpostService;
 	private final TilknyttedeJournalposterService tilknyttedeJournalposterService;
 
-	public HentJournalsakinfoController(SafHentDokumentService safHentDokumentService,
-										SafHentJournalpostService safHentJournalpostService,
+	public HentJournalsakinfoController(SafHentJournalpostService safHentJournalpostService,
 										FinnJournalposterService finnJournalposterService,
 										FinnJournalposterStatusService finnJournalposterStatusService,
 										HentTilgangJournalpostService hentTilgangJournalpostService,
 										TilknyttedeJournalposterService tilknyttedeJournalposterService) {
-		this.safHentDokumentService = safHentDokumentService;
 		this.safHentJournalpostService = safHentJournalpostService;
 		this.finnJournalposterService = finnJournalposterService;
 		this.finnJournalposterStatusService = finnJournalposterStatusService;
@@ -119,22 +106,5 @@ public class HentJournalsakinfoController {
 		FinnJournalposterStatusResponseTo finnJournalposterStatusResponseTo = finnJournalposterStatusService.finnJournalposterStatus(finnJournalposterStatusRequestTo);
 		log.info("rjoark904 fant og returnerer {} journalposter med request={}.", finnJournalposterStatusResponseTo.getTilgangJournalposter().size(), finnJournalposterStatusRequestTo);
 		return finnJournalposterStatusResponseTo;
-	}
-
-	@Transactional(readOnly = true)
-	@RequestMapping(value = "/hentdokument/{dokumentinfoId}/{variant}")
-	@RestMetrics(value = "dok_request", extraTags = {"process_code", RJOARK_920}, percentiles = {0.5, 0.95})
-	public ResponseEntity<String> safHentDokument(@PathVariable Long dokumentinfoId,
-												  @PathVariable VariantFormatCode variant) {
-
-		RequestContextUtil.createAndSetUsername(RJOARK_920, "dokarkiv");
-		log.info("rjoark920 har mottatt forespørsel om dokument med dokumentinfoId={} og variant={}", dokumentinfoId, variant);
-		SafHentDokumentResponse safHentDokumentResponse = safHentDokumentService.hentDokumentByDokumentinfoIdAndVariant(dokumentinfoId, variant);
-		String mimeTypeForFileExtension = mimeTypeMapper.getMimeTypeForFileExtension(safHentDokumentResponse.getFiltype());
-		log.info("rjoark920 har hentet dokument med dokumentinfoId={} og variant={}, Content-Type={}", dokumentinfoId, variant, mimeTypeForFileExtension);
-
-		return ResponseEntity.ok()
-				.header(HttpHeaders.CONTENT_TYPE, mimeTypeForFileExtension)
-				.body(Base64.getEncoder().encodeToString(safHentDokumentResponse.getDokument()));
 	}
 }
