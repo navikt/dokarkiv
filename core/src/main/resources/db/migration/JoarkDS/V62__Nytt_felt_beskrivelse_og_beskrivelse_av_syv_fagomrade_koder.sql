dfa24e709000322c83bd6de3dea44719e150fe19
Kristian Laskemoen <kristian.laskemoen@nav.no>
2023-10-12 14:34:35 +0200

    rettet sql (og fjernet kjøring fra schema_version i db)

--- core/src/main/resources/db/migration/JoarkDS/V62__Nytt_felt_beskrivelse_og_beskrivelse_av_syv_fagomrade_koder.sql
+++ core/src/main/resources/db/migration/JoarkDS/V62__Nytt_felt_beskrivelse_og_beskrivelse_av_syv_fagomrade_koder.sql
@@ -1,32 +1,25 @@
 alter table T_K_FAGOMRADE
     add (
-        beskrivelse VARCHAR2(200),
+        beskrivelse VARCHAR2(200)
         );
 
-insert into T_K_FAGOMRADE (beskrivelse)
-VALUES ('NAV utreder og belyser saken på forespørsel fra Statens sivilrettsforvaltning')
+update T_K_FAGOMRADE set beskrivelse = 'NAV utreder og belyser saken på forespørsel fra Statens sivilrettsforvaltning'
 where k_fagomrade='RVE';
 
-insert into T_K_FAGOMRADE (beskrivelse)
-VALUES ('Krav om økonomisk erstatning fordi NAV har gjort en feil')
+update T_K_FAGOMRADE set beskrivelse = 'Krav om økonomisk erstatning fordi NAV har gjort en feil'
 where k_fagomrade='ERS';
 
-insert into T_K_FAGOMRADE (beskrivelse)
-VALUES ('Dokumentasjon knyttet til NAVs rekrutteringsbistand til arbeidsgivere')
+update T_K_FAGOMRADE set beskrivelse = 'Dokumentasjon knyttet til NAVs rekrutteringsbistand til arbeidsgivere'
 where k_fagomrade='REK';
 
-insert into T_K_FAGOMRADE (beskrivelse)
-VALUES ('Intensjonsavtalen om et mer inkluderende arbeidsliv: Samarbeidsavtaler, mål- og handlingsplaner. Noe tilskudd',)
+update T_K_FAGOMRADE set beskrivelse = 'Intensjonsavtalen om et mer inkluderende arbeidsliv: Samarbeidsavtaler, mål- og handlingsplaner. Noe tilskudd'
 where k_fagomrade='IAR';
 
-insert into T_K_FAGOMRADE (beskrivelse)
-VALUES ('Endring av bankkonto eller midlertidige adresser')
+update T_K_FAGOMRADE set beskrivelse = 'Endring av bankkonto eller midlertidige adresser'
 where k_fagomrade='AGR';
 
-insert into T_K_FAGOMRADE (beskrivelse)
-VALUES ('Vedtak om stans av sykepenger, og behandling av klager og anker')
+update T_K_FAGOMRADE set beskrivelse = 'Vedtak om stans av sykepenger, og behandling av klager og anker'
 where k_fagomrade='SAP';
 
-insert into T_K_FAGOMRADE (beskrivelse)
-VALUES ('Samhandling mellom NAV og arbeidsgivere, utover det som omfattes av øvrige fagområder')
+update T_K_FAGOMRADE set beskrivelse = 'Samhandling mellom NAV og arbeidsgivere, utover det som omfattes av øvrige fagområder'
 where k_fagomrade='OPA';
