01cdd43cda9d3078d618a1e48ac96cd7d4099448
Joakim Bjørnstad <joakim.bjornstad@nav.no>
2023-10-16 18:46:43 +0200

    MMA-7093 La til magic number sjekk på flere typer PDF

--- journalpost/src/test/java/no/nav/dokarkiv/journalpost/v1/validators/FilMagicNumberValidatorTest.java
+++ journalpost/src/test/java/no/nav/dokarkiv/journalpost/v1/validators/FilMagicNumberValidatorTest.java
@@ -1,12 +1,14 @@
 package no.nav.dokarkiv.journalpost.v1.validators;
 
 import no.nav.dokarkiv.core.domain.codes.FilTypeCode;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.core.io.ClassPathResource;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.HexFormat;
 import java.util.stream.Stream;
 
@@ -14,6 +16,7 @@ import static no.nav.dokarkiv.core.domain.codes.FilTypeCode.JPEG;
 import static no.nav.dokarkiv.core.domain.codes.FilTypeCode.PDF;
 import static no.nav.dokarkiv.core.domain.codes.FilTypeCode.PNG;
 import static no.nav.dokarkiv.core.domain.codes.FilTypeCode.XML;
+import static no.nav.dokarkiv.journalpost.v1.validators.FilMagicNumberValidator.PDF_MAGIC_NUMBER;
 import static no.nav.dokarkiv.journalpost.v1.validators.FilMagicNumberValidator.isFileContentContainsValidMagicNumber;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -54,6 +57,15 @@ class FilMagicNumberValidatorTest {
 				Arguments.of(HexFormat.of().parseHex("230000000C6A5020200D0A870AFFD8FFEE"), JPEG),
 				Arguments.of(pngFile, PNG)
 		);
+	}
+
+	@Test
+	void shouldReturnTrueWhenPdfHasMagicNumberInFirst1024Bytes() {
+		byte[] pdfFile = new byte[1024];
+		Arrays.fill(pdfFile, 0, 1024, (byte) 0x0);
+		System.arraycopy(PDF_MAGIC_NUMBER, 0, pdfFile, 1024 - PDF_MAGIC_NUMBER.length, PDF_MAGIC_NUMBER.length);
+
+		assertThat(isFileContentContainsValidMagicNumber("PDF", pdfFile)).isTrue();
 	}
 
 	private static byte[] classpathToInputStream(String classpathResource) throws IOException {
