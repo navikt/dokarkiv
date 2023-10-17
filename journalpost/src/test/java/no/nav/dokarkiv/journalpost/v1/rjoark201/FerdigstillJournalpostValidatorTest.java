9c2c36b8cea8dbe0f331da2c128e7fc7ace71e3b
Kristian Laskemoen <kristian.laskemoen@nav.no>
2023-10-17 14:58:13 +0200

    Revert "Et sett med stringer som også kan hentes fra konstanter. og rydde to warninger"
    This reverts commit 8cadee92dcf746a11659d198752f684b76455612.

--- journalpost/src/test/java/no/nav/dokarkiv/journalpost/v1/rjoark201/FerdigstillJournalpostValidatorTest.java
+++ journalpost/src/test/java/no/nav/dokarkiv/journalpost/v1/rjoark201/FerdigstillJournalpostValidatorTest.java
@@ -14,7 +14,6 @@ import no.nav.dokarkiv.core.exceptions.KanIkkeFerdigstilleException;
 import no.nav.dokarkiv.journalpost.v1.validators.FerdigstillJournalpostValidator;
 import org.junit.jupiter.api.Test;
 
-import static no.nav.dokarkiv.core.aksjonslogg.ArkivElementConstants.JOURNALPOST_AVSENDER_MOTTAKER;
 import static no.nav.dokarkiv.core.aksjonslogg.ArkivElementConstants.JOURNALPOST_FAGOMRADE;
 import static no.nav.dokarkiv.core.aksjonslogg.ArkivElementConstants.JOURNALPOST_INNHOLD;
 import static no.nav.dokarkiv.core.util.TestDataUtils.createJournalpost;
@@ -120,7 +119,7 @@ public class FerdigstillJournalpostValidatorTest {
 
 		assertThrows(KanIkkeFerdigstilleException.class,
 				() -> validator.validatePaakrevdeFelter(journalpost),
-				JOURNALPOST_AVSENDER_MOTTAKER);
+				"Journalpost.avsendMottaker");
 	}
 
 	@Test
