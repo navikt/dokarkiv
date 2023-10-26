cbc293cd5423268c7d638657c7ac8c176dda2376
Joakim Bjørnstad <joakim.bjornstad@nav.no>
2023-10-26 15:32:40 +0200

    MMA-7081 Fjernet StsRestConsumer og config
    * Brukes ikke lenger

--- core/src/main/java/no/nav/dokarkiv/core/consumer/sts/StsRequest.java
+++
@@ -1,9 +0,0 @@
-package no.nav.dokarkiv.core.consumer.sts;
-
-import lombok.Getter;
-
-@Getter
-public class StsRequest {
-    private final String grant_type = "client_credentials";
-    private final String scope = "openid";
-}
