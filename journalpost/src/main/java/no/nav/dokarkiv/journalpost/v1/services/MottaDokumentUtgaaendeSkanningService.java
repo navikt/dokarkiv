cdbba52e0de09a070ef9dda948bb9fcc32100f43
Joakim Bjørnstad <joakim.bjornstad@nav.no>
2023-10-26 16:23:55 +0200

    MMA-7081 Fikset GHAS code scanning alert

--- journalpost/src/main/java/no/nav/dokarkiv/journalpost/v1/services/MottaDokumentUtgaaendeSkanningService.java
+++ journalpost/src/main/java/no/nav/dokarkiv/journalpost/v1/services/MottaDokumentUtgaaendeSkanningService.java
@@ -22,7 +22,6 @@ import no.nav.dokarkiv.journalpost.v1.validators.MottaDokumentUtgaaendeSkanningV
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static no.nav.dokarkiv.core.MDCConstants.MDC_REQUEST_ID;
 import static org.slf4j.MDC.get;
@@ -31,104 +30,92 @@ import static org.slf4j.MDC.get;
 @Slf4j
 public class MottaDokumentUtgaaendeSkanningService {
 
-    private final JournalpostRepository journalpostRepository;
-    private final DokumentFilRepository dokumentFilRepository;
-    private final MottaDokumentUtgaaendeSkanningValidator validator = new MottaDokumentUtgaaendeSkanningValidator();
+	private final JournalpostRepository journalpostRepository;
+	private final DokumentFilRepository dokumentFilRepository;
+	private final MottaDokumentUtgaaendeSkanningValidator validator = new MottaDokumentUtgaaendeSkanningValidator();
 
-    private final String KILDENAVN = "skanmotutgaaende";
-    private final String JOURNALPOST = "journalpost";
-    private final String REQUEST = "request";
+	private final String KILDENAVN = "skanmotutgaaende";
+	private final String JOURNALPOST = "journalpost";
+	private final String REQUEST = "request";
 
-    public MottaDokumentUtgaaendeSkanningService(JournalpostRepository journalpostRepository, DokumentFilRepository dokumentFilRepository) {
-        this.journalpostRepository = journalpostRepository;
-        this.dokumentFilRepository = dokumentFilRepository;
-    }
+	public MottaDokumentUtgaaendeSkanningService(JournalpostRepository journalpostRepository, DokumentFilRepository dokumentFilRepository) {
+		this.journalpostRepository = journalpostRepository;
+		this.dokumentFilRepository = dokumentFilRepository;
+	}
 
-    public void mottaDokumentUtgaaendeSkanning(Long journalpostId, MottaDokumentUtgaaendeSkanningRequest request) throws DokarkivFunctionalException, DokarkivTechnicalException {
-        try {
+	public void mottaDokumentUtgaaendeSkanning(Long journalpostId, MottaDokumentUtgaaendeSkanningRequest request) throws DokarkivFunctionalException, DokarkivTechnicalException {
+		try {
+			validateRequest(journalpostId, request);
 
-            validateRequest(journalpostId, request);
+			Journalpost journalpost = journalpostRepository.findById(journalpostId)
+					.orElseThrow(() -> new JournalpostIkkeFunnetException(get(MDC_REQUEST_ID) + " journalpostId=" + journalpostId + " ikke funnet i databasen"));
+			validateJournalpost(journalpostId, request, journalpost);
 
-            Journalpost journalpost = journalpostRepository.findById(journalpostId).orElseThrow(() -> new JournalpostIkkeFunnetException(get(MDC_REQUEST_ID) + "\n" + "journalpost med id " + journalpostId + " ikke funnet"));
-            validateJournalpost(journalpostId, request, journalpost);
+			journalpost.setJournalstatus(JournalStatusCode.FL);
 
-            journalpost.setJournalstatus(JournalStatusCode.FL);
+			request.getTilleggsopplysninger().forEach(tilleggsopplysning -> journalpost.getTilleggsopplysninger().put(tilleggsopplysning.getNokkel(), tilleggsopplysning.getVerdi()));
 
-            request.getTilleggsopplysninger().forEach(tilleggsopplysning -> journalpost.getTilleggsopplysninger().put(tilleggsopplysning.getNokkel(), tilleggsopplysning.getVerdi()));
+			journalpost.setMottakskanal(MottaksKanalCode.valueOf(request.getMottakskanal()));
 
-            journalpost.setMottakskanal(MottaksKanalCode.valueOf(request.getMottakskanal()));
+			journalpost.setEndretKildeNavn(KILDENAVN);
+			if (request.getDatoMottatt() != null) {
+				journalpost.setMottattDato(request.getDatoMottatt());
+			}
+			journalpost.setJournalDato(DateProvider.getToday());
 
-            journalpost.setEndretKildeNavn(KILDENAVN);
-            if (request.getDatoMottatt() != null) {
-                journalpost.setMottattDato(request.getDatoMottatt());
-            }
-            journalpost.setJournalDato(DateProvider.getToday());
+			List<FilDetaljer> filDetaljerList = request.getDokumentvarianter()
+					.stream()
+					.map(dokumentVariant -> mapDokumentVariantToFildetaljer(dokumentVariant, request.getBatchnavn()))
+					.toList();
 
-            List<FilDetaljer> filDetaljerList = request.getDokumentvarianter()
-                    .stream()
-                    .map(dokumentVariant -> mapDokumentVariantToFildetaljer(dokumentVariant, request.getBatchnavn()))
-                    .collect(Collectors.toList());
+			filDetaljerList.forEach(filDetaljer -> {
+				DokumentFil dokumentFil = filDetaljer.createDokumentFil();
+				dokumentFilRepository.persist(dokumentFil);
+			});
+			filDetaljerList.forEach(filDetaljer -> journalpost.findHoveddokumentDokumentInfoRelasjon().getDokumentInfo().addFilDetaljer(filDetaljer));
 
-            filDetaljerList.forEach(filDetaljer -> {
-                DokumentFil dokumentFil = filDetaljer.createDokumentFil();
-                dokumentFilRepository.persist(dokumentFil);
-            });
-            filDetaljerList.forEach(filDetaljer -> journalpost.findHoveddokumentDokumentInfoRelasjon().getDokumentInfo().addFilDetaljer(filDetaljer));
+		} catch (Exception e) {
+			if (!(e instanceof DokarkivFunctionalException || e instanceof DokarkivTechnicalException)) {
+				log.error("{} mottaDokumentUtgaaendeSkanning feilet med ukjent feil på journalpost. journalpostId={}, mottakskanal={}, batchnavn={}, feilmelding={}",
+						get(MDC_REQUEST_ID), journalpostId, request.getMottakskanal(), request.getBatchnavn(), e.getMessage(), e);
+			}
+			throw e;
+		}
+	}
 
-        } catch (Exception e) {
-            if (!(e instanceof DokarkivFunctionalException || e instanceof DokarkivTechnicalException)) {
-                log.error(
-                        get(MDC_REQUEST_ID) + " mottaDokumentUtgaaendeSkanning feilet med ukjent feil på journalpost "
-                                + "journalpostId=" + journalpostId + " "
-                                + "mottakskanal=" + request.getMottakskanal() + " "
-                                + "batchnavn=" + request.getBatchnavn() + " "
-                                + "feilmedling=" + e.getMessage(), e
-                );
-            }
-            throw e;
-        }
-    }
+	private void validateRequest(Long journalpostId, MottaDokumentUtgaaendeSkanningRequest request) throws InputValideringFeiletException {
+		validator.validateRequest(request).ifPresent(errors -> {
+			throw new InputValideringFeiletException(generateErrorMessage(errors, journalpostId, request, REQUEST));
+		});
+	}
 
-    private void validateRequest(Long journalpostId, MottaDokumentUtgaaendeSkanningRequest request) throws InputValideringFeiletException {
-        validator.validateRequest(request).ifPresent(errors -> {
-            throw new InputValideringFeiletException(generateErrorMessage(errors, journalpostId, request, REQUEST));
-        });
-    }
+	private void validateJournalpost(Long journalpostId, MottaDokumentUtgaaendeSkanningRequest request, Journalpost journalpost) throws DokarkivFunctionalException {
+		validator.validateJournalpostHasAllElements(journalpost).ifPresent(errors -> {
+			throw new InputValideringFeiletException(generateErrorMessage(errors, journalpostId, request, JOURNALPOST));
+		});
 
-    private void validateJournalpost(Long journalpostId, MottaDokumentUtgaaendeSkanningRequest request, Journalpost journalpost) throws DokarkivFunctionalException {
-        validator.validateJournalpostHasAllElements(journalpost).ifPresent(errors -> {
-            throw new InputValideringFeiletException(generateErrorMessage(errors, journalpostId, request, JOURNALPOST));
-        });
+		validator.validateJournalpostMetadata(journalpost).ifPresent(errors -> {
+			throw new InputValideringBadMetadataException(generateErrorMessage(errors, journalpostId, request, JOURNALPOST));
+		});
+	}
 
-        validator.validateJournalpostMetadata(journalpost).ifPresent(errors -> {
-            throw new InputValideringBadMetadataException(generateErrorMessage(errors, journalpostId, request, JOURNALPOST));
-        });
-    }
+	private String generateErrorMessage(String errors, Long journalpostId, MottaDokumentUtgaaendeSkanningRequest request, String valideringAv) {
+		return String.format("%s feilet ved validering av %s. journalpostId=%d, mottakskanal=%s, batchnavn=%s, feilmelding=%s",
+				get(MDC_REQUEST_ID), valideringAv, journalpostId, request.getMottakskanal(), request.getBatchnavn(), errors);
+	}
 
-    private String generateErrorMessage(String errors, Long journalpostId, MottaDokumentUtgaaendeSkanningRequest request, String valideringAv) {
-        String errorMessage = String.format(get(MDC_REQUEST_ID) + " feilet ved validering av %s "
-                + "journalpostId=" + journalpostId + " "
-                + "mottakskanal=" + request.getMottakskanal() + " "
-                + "batchnavn=" + request.getBatchnavn() + " "
-                + "feilmedling=" + errors, valideringAv);
-        return errorMessage;
-    }
+	private FilDetaljer mapDokumentVariantToFildetaljer(DokumentVariant dokumentVariant, String batchnavn) {
+		FilDetaljer filDetaljer = FilDetaljer
+				.builder()
+				.filtype(FilTypeCode.valueOf(dokumentVariant.getFiltype()))
+				.filnavn(dokumentVariant.getFilnavn())
+				.variantFormat(VariantFormatCode.valueOf(dokumentVariant.getVariantformat()))
+				.fileContent(dokumentVariant.getFysiskDokument())
+				.batchNavn(batchnavn)
+				.filUuid(FilDetaljer.generateUuid())
+				.build();
+		filDetaljer.setOpprettetKildeNavn(KILDENAVN);
+		return filDetaljer;
+	}
 
-    private FilDetaljer mapDokumentVariantToFildetaljer(DokumentVariant dokumentVariant, String batchnavn) {
-        FilDetaljer filDetaljer = FilDetaljer
-                .builder()
-                .filtype(FilTypeCode.valueOf(dokumentVariant.getFiltype()))
-                .filnavn(dokumentVariant.getFilnavn())
-                .variantFormat(VariantFormatCode.valueOf(dokumentVariant.getVariantformat()))
-                .fileContent(dokumentVariant.getFysiskDokument())
-                .batchNavn(batchnavn)
-                .filUuid(FilDetaljer.generateUuid())
-                .build();
-        filDetaljer.setOpprettetKildeNavn(KILDENAVN);
-        return filDetaljer;
-    }
-
-    private boolean notNullOrEmpty(String string) {
-        return string != null && !string.isBlank();
-    }
 }
