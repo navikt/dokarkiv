79515602af8266fd9225a4de3518f8bd096929fb
Kristian Laskemoen <kristian.laskemoen@nav.no>
2024-02-09 13:15:08 +0100

    Justert litt spørringene og lagt til fremmednøkkel på K_BRUKER_ID_T

--- core/src/main/resources/db/migration/JoarkDS/V67__Ny_tabell_T_K_BRUKER_ID_T.sql
+++ core/src/main/resources/db/migration/JoarkDS/V67__Ny_tabell_T_K_BRUKER_ID_T.sql
@@ -1,18 +1,19 @@
 CREATE TABLE T_K_BRUKER_ID_T
 (
-    K_BRUKER_ID_T    VARCHAR2(40)  NOT NULL
-        CONSTRAINT T_K_BRUKER_ID_T
+    K_BRUKER_ID_T VARCHAR2(40)  NOT NULL
         PRIMARY KEY,
-    DEKODE         VARCHAR2(200) NOT NULL,
-    OPPRETTET_AV   VARCHAR2(20)  NOT NULL,
-    DATO_ENDRET    TIMESTAMP(6)  NULL,
-    ENDRET_AV      VARCHAR2(20)  NULL
+    DEKODE        VARCHAR2(200) NOT NULL,
+    OPPRETTET_AV  VARCHAR2(20)  NOT NULL,
+    DATO_ENDRET   TIMESTAMP(6)  NULL,
+    ENDRET_AV     VARCHAR2(20)  NULL
 );
 
-Insert Into T_K_BRUKER_ID_T (K_BRUKER_ID_T, DEKODE, OPPRETTET_AV )
-VALUES ('FNR', 'Identitetsnummer i Folkeregisteret. Kan være fødselsnummer eller D-nummer. Identifiserer personer som har tilknytning til Norge',
+INSERT INTO T_K_BRUKER_ID_T (K_BRUKER_ID_T, DEKODE, OPPRETTET_AV)
+VALUES ('FNR',
+        'Identitetsnummer i Folkeregisteret. Kan være fødselsnummer eller D-nummer. Identifiserer personer som har tilknytning til Norge',
         'MMA-7293');
 
-Insert Into T_K_BRUKER_ID_T (K_BRUKER_ID_T, DEKODE, OPPRETTET_AV )
-VALUES ('ORGNR', 'Organisasjonsnummer i Enhetsregisteret og Foretaksregisteret. Identifiserer juridiske personer som bedrifter i Norge',
+INSERT INTO T_K_BRUKER_ID_T (K_BRUKER_ID_T, DEKODE, OPPRETTET_AV)
+VALUES ('ORGNR',
+        'Organisasjonsnummer i Enhetsregisteret og Foretaksregisteret. Identifiserer juridiske personer som bedrifter i Norge',
         'MMA-7293');
