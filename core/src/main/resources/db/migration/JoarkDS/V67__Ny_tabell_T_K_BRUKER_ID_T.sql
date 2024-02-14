83126e84ca9fb3023bbd9c82467821999da52528
Kristian Laskemoen <61700528+L145025@users.noreply.github.com>
2024-02-14 06:14:49 +0100

    Mma 7293: Legge til kolonner til Sak, ny tabell T_K_BRUKER_ID_T og slette T_SAK_STATUS (#571)

---
+++ core/src/main/resources/db/migration/JoarkDS/V67__Ny_tabell_T_K_BRUKER_ID_T.sql
@@ -0,0 +1,20 @@
+CREATE TABLE T_K_BRUKER_ID_T
+(
+    K_BRUKER_ID_T  VARCHAR2(40)  NOT NULL
+        PRIMARY KEY,
+    DEKODE         VARCHAR2(200) NOT NULL,
+    DATO_OPPRETTET TIMESTAMP(6)  NULL,
+    OPPRETTET_AV   VARCHAR2(20)  NOT NULL,
+    DATO_ENDRET    TIMESTAMP(6)  NULL,
+    ENDRET_AV      VARCHAR2(20)  NULL
+);
+
+INSERT INTO T_K_BRUKER_ID_T (K_BRUKER_ID_T, DEKODE, OPPRETTET_AV)
+VALUES ('FNR',
+        'Identitetsnummer i Folkeregisteret. Kan være fødselsnummer eller D-nummer. Identifiserer personer som har tilknytning til Norge',
+        'MMA-7293');
+
+INSERT INTO T_K_BRUKER_ID_T (K_BRUKER_ID_T, DEKODE, OPPRETTET_AV)
+VALUES ('ORGNR',
+        'Organisasjonsnummer i Enhetsregisteret og Foretaksregisteret. Identifiserer juridiske personer som bedrifter i Norge',
+        'MMA-7293');
