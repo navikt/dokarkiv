9fc0470cd826f50f9ab4f3ac0a575814e9ce3738
Tsigab <tsigab.angosom.gebremedhin@nav.no>
2024-06-26 10:00:18 +0200

    MMA-7410 oppryddet mocking av AzureAdGraphService i itest.

--- arkiverdokumentproduksjon/src/test/java/no/nav/dokarkiv/arkiverdokumentproduksjon/AbstractArkiverdokumentproduksjonItest.java
+++ arkiverdokumentproduksjon/src/test/java/no/nav/dokarkiv/arkiverdokumentproduksjon/AbstractArkiverdokumentproduksjonItest.java
@@ -1,13 +1,11 @@
 package no.nav.dokarkiv.arkiverdokumentproduksjon;
 
 import no.nav.dokarkiv.core.CoreConfig;
-import no.nav.dokarkiv.core.consumer.azure.AzureAdGraphService;
 import no.nav.dokarkiv.core.repository.DokumentFilTestRepository;
 import no.nav.dokarkiv.core.repository.DokumentInfoRepository;
 import no.nav.dokarkiv.core.repository.JournalpostTestRepository;
 import no.nav.dokarkiv.core.stelvio.RequestContextSetter;
 import no.nav.dokarkiv.core.stelvio.SimpleRequestContext;
-import no.nav.dokarkiv.core.storage.GoogleCloudBucketStorage;
 import no.nav.security.token.support.spring.test.EnableMockOAuth2Server;
 import no.nav.tjeneste.domene.brevogarkiv.arkiverdokumentproduksjon.v1.ArkiverDokumentproduksjonV1;
 import org.junit.jupiter.api.BeforeEach;
@@ -15,18 +13,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureTestEntityManager;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.test.context.ActiveProfiles;
 
 import javax.transaction.Transactional;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE,
-		classes = {AbstractArkiverdokumentproduksjonItest.Config.class, CoreConfig.class,
+		classes = {CoreConfig.class,
 				ArkiverDokumentproduksjonConfig.class},
 		properties = {"spring.main.allow-bean-definition-overriding=true"})
 @ActiveProfiles("itest")
@@ -56,19 +48,5 @@ public abstract class AbstractArkiverdokumentproduksjonItest {
 				.userId(ITEST_USERID)
 				.componentId(ITEST_COMPONENTID)
 				.build());
-	}
-
-	@Configuration
-	static class Config {
-		@Bean
-		public GoogleCloudBucketStorage dokprodMellomlagerStorage() {
-			return mock(GoogleCloudBucketStorage.class);
-		}
-		@Bean
-		public AzureAdGraphService azureAdGraphService() {
-			AzureAdGraphService azureAdGraphServiceMock = mock(AzureAdGraphService.class);
-			when(azureAdGraphServiceMock.hentFulltNavn(any())).thenReturn("Username");
-			return azureAdGraphServiceMock;
-		}
 	}
 }
--- safintern/src/test/java/no/nav/dokarkiv/safintern/AbstractSafinternTest.java
+++ safintern/src/test/java/no/nav/dokarkiv/safintern/AbstractSafinternTest.java
@@ -3,41 +3,24 @@ package no.nav.dokarkiv.safintern;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import no.nav.dokarkiv.core.AbstractRestIT;
 import no.nav.dokarkiv.core.CoreConfig;
-import no.nav.dokarkiv.core.consumer.azure.AzureAdGraphService;
 import no.nav.dokarkiv.core.repository.DokumentFilTestRepository;
 import no.nav.dokarkiv.core.repository.DokumentInfoRepository;
-import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpEntity;
 import org.springframework.test.context.ActiveProfiles;
 
 import static no.nav.dokarkiv.safintern.SafinternConstants.ROLE_CLAIM_TILGANG;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
 @SpringBootTest(
 		webEnvironment = RANDOM_PORT,
-		classes = {CoreConfig.class, SafinternConfig.class, AbstractSafinternTest.Config.class},
+		classes = {CoreConfig.class, SafinternConfig.class},
 		properties = {"spring.main.allow-bean-definition-overriding=true"}
 )
 @ActiveProfiles("itest")
 public abstract class AbstractSafinternTest extends AbstractRestIT {
-
-	@Configuration
-	static class Config {
-		@Bean
-		public AzureAdGraphService azureAdGraphService() {
-			AzureAdGraphService azureAdGraphServiceMock = mock(AzureAdGraphService.class);
-			when(azureAdGraphServiceMock.hentFulltNavn(any())).thenReturn("Username");
-			return azureAdGraphServiceMock;
-		}
-	}
 
 	@Autowired
 	protected TestRestTemplate restTemplate;
