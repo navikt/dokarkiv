cb6aa182a0ca32aa60dbcd2f31988559f6516775
Kristian Laskemoen <kristian.laskemoen@nav.no>
2024-10-04 14:19:14 +0200

    #228, saniterer loginnslag som gjøres før håndtering av request

--- safintern/src/main/java/no/nav/dokarkiv/safintern/journalstatus/FinnJournalposterStatusController.java
+++ safintern/src/main/java/no/nav/dokarkiv/safintern/journalstatus/FinnJournalposterStatusController.java
@@ -3,6 +3,7 @@ package no.nav.dokarkiv.safintern.journalstatus;
 import lombok.extern.slf4j.Slf4j;
 import no.nav.dokarkiv.core.domain.codes.JournalStatusCode;
 import no.nav.dokarkiv.core.domain.codes.JournalpostTypeCode;
+import no.nav.dokarkiv.core.util.SafeLoggingUtil;
 import no.nav.dokarkiv.safintern.views.PaginatedJournalpostView;
 import no.nav.security.token.support.core.api.ProtectedWithClaims;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import static no.nav.dokarkiv.core.security.SporingHandlerInterceptor.ISSUER_AZUREV2;
 import static no.nav.dokarkiv.safintern.SafinternConstants.BASE_PATH;
@@ -36,7 +38,8 @@ public class FinnJournalposterStatusController {
 															@RequestParam(required = false) Set<String> fields) {
 		JournalStatusCode journalstatus = finnJournalposterStatusRequest.journalstatus();
 		List<JournalpostTypeCode> journalposttyper = finnJournalposterStatusRequest.journalposttyper();
-		log.info("safintern/finnjournalposterstatus har mottatt kall om journalposter med status={}, typer={}, fields={}", journalstatus, journalposttyper, fields);
+		Set<String> logFields = fields == null ? null :	fields.stream().map(SafeLoggingUtil::removeUnsafeChars).collect(Collectors.toSet());
+		log.info("safintern/finnjournalposterstatus har mottatt kall om journalposter med status={}, typer={}, fields={}",journalstatus , journalposttyper, logFields);
 		var journalpostsView = safinternJournalStatusService.finnJournalposterStatus(finnJournalposterStatusRequest, fields);
 		log.info("safintern/finnjournalposterstatus hentet journalposter med status={}. typer={}, side {} av {}", journalstatus, journalposttyper, journalpostsView.page(), journalpostsView.totalPages());
 		return journalpostsView;
