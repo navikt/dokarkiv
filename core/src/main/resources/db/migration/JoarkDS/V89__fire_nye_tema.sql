dc8809428f5aaa188e03f3384152c64941795b2b
Joakim Bjørnstad <joakim.bjornstad@nav.no>
2025-04-29 13:16:06 +0200

    MMA-8059 La til nye tema: AKT, UNG, PAI, POI
    * Databasen og enum

---
+++ core/src/main/resources/db/migration/JoarkDS/V89__fire_nye_tema.sql
@@ -0,0 +1,16 @@
+insert into t_k_fagomrade (k_fagomrade, dekode, dato_fom, dato_tom, er_gyldig, dato_opprettet, opprettet_av,
+                           dato_endret, endret_av, avlever_med_dok)
+values ('AKT', 'Aktivitetsplan med dialoger', date '2025-04-29', null, '1', current_timestamp,
+        'MMA-8059', current_timestamp, 'MMA-8059', '1');
+insert into t_k_fagomrade (k_fagomrade, dekode, dato_fom, dato_tom, er_gyldig, dato_opprettet, opprettet_av,
+                           dato_endret, endret_av, avlever_med_dok)
+values ('UNG', 'Ungdomsprogramytelsen', date '2025-04-29', null, '1', current_timestamp,
+        'MMA-8059', current_timestamp, 'MMA-8059', '1');
+insert into t_k_fagomrade (k_fagomrade, dekode, dato_fom, dato_tom, er_gyldig, dato_opprettet, opprettet_av,
+                           dato_endret, endret_av, avlever_med_dok)
+values ('PAI', 'Innsyn', date '2025-04-29', null, '1', current_timestamp,
+        'MMA-8059', current_timestamp, 'MMA-8059', '1');
+insert into t_k_fagomrade (k_fagomrade, dekode, dato_fom, dato_tom, er_gyldig, dato_opprettet, opprettet_av,
+                           dato_endret, endret_av, avlever_med_dok)
+values ('POI', 'Innsyn etter personopplysningsloven', date '2025-04-29', null, '1', current_timestamp,
+        'MMA-8059', current_timestamp, 'MMA-8059', '1');
