bed72ecb89a5dc24a406297c6e290d686f5494a7
Anders Opskar Voldsund <anders.voldsund@nav.no>
2026-03-19 15:42:47 +0100

    Oppdater Slettebestilling-entiteten så det stemmer med flywayskripta i V102/V105
    - nullable = false for dato_utfores og opprettet_av_navn

--- core/src/main/java/no/nav/dokarkiv/core/domain/entities/Slettebestilling.java
+++ core/src/main/java/no/nav/dokarkiv/core/domain/entities/Slettebestilling.java
@@ -2,7 +2,6 @@ package no.nav.dokarkiv.core.domain.entities;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
@@ -19,10 +18,10 @@ import no.nav.dokarkiv.core.domain.codes.SlettebestillingHjemmelCode;
 import no.nav.dokarkiv.core.domain.codes.SlettebestillingStatusCode;
 import no.nav.dokarkiv.core.domain.codes.SlettebestillingTypeCode;
 
-import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
+import static jakarta.persistence.EnumType.STRING;
 import static jakarta.persistence.GenerationType.SEQUENCE;
 
 @Getter
@@ -34,6 +33,7 @@ import static jakarta.persistence.GenerationType.SEQUENCE;
 public class Slettebestilling extends AbstractPersistentVersionedDomainObjectWithKilde {
 
 	public static final int SLETTEBESTILLING_BEGRUNNELSE_MAX_LENGTH = 512;
+	private static final int LARGE_LENGTH = 512;
 	private static final String SLETTEBESTILLING_SEQUENCE = "slettebestilling_seq";
 	private static final String DATABASE_SLETTEBESTILLING_SEQUENCE = "t_slettebestilling_seq";
 
@@ -42,33 +42,43 @@ public class Slettebestilling extends AbstractPersistentVersionedDomainObjectWit
 	@SequenceGenerator(name = SLETTEBESTILLING_SEQUENCE, sequenceName = DATABASE_SLETTEBESTILLING_SEQUENCE)
 	@Column(name = "slettebestilling_id", nullable = false)
 	private long slettebestillingId;
-	@Enumerated(EnumType.STRING)
+
+	@Enumerated(STRING)
 	@Column(name = "k_slettebestilling_type", nullable = false)
 	private SlettebestillingTypeCode slettebestillingType;
-	@Enumerated(EnumType.STRING)
+
+	@Enumerated(STRING)
 	@Column(name = "k_slettebestilling_status", nullable = false)
 	private SlettebestillingStatusCode slettebestillingStatus;
-	@Enumerated(EnumType.STRING)
+
+	@Enumerated(STRING)
 	@Column(name = "k_slettebestilling_hjemmel", nullable = false)
 	private SlettebestillingHjemmelCode slettebestillingHjemmel;
-	@Enumerated(EnumType.STRING)
+
+	@Enumerated(STRING)
 	@Column(name = "k_slettebestilling_arsak", nullable = false)
 	private SlettebestillingArsakCode slettebestillingArsak;
+
 	@Column(name = "begrunnelse", length = SLETTEBESTILLING_BEGRUNNELSE_MAX_LENGTH)
 	private String begrunnelse;
+
 	@Column(name = "dokument_info_id")
 	private Long dokumentInfoId;
+
 	@Column(name = "sak_id")
 	private Long sakId;
-	@Column(name = "dato_utfores")
+
+	@Column(name = "dato_utfores", nullable = false)
 	private LocalDate datoUtfores;
+
 	@Column(name = "dato_utfort")
 	private LocalDateTime datoUtfort;
-	@Column(name = "opprettet_av_navn", length = 512)
-	private String opprettetAvNavn;
-	@Column(name = "endret_av_navn", length = 512)
-	private String endretAvNavn;
 
+	@Column(name = "opprettet_av_navn", nullable = false, length = LARGE_LENGTH)
+	private String opprettetAvNavn;
+
+	@Column(name = "endret_av_navn", length = LARGE_LENGTH)
+	private String endretAvNavn;
 
 	@Override
 	public Long getId() {
