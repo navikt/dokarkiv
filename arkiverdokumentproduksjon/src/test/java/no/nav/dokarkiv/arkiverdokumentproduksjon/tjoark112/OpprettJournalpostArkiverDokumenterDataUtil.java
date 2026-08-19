c51ec454a3cadc070c78598e309b39a0f34ff5c2
Joakim Bjørnstad <joakim.bjornstad@nav.no>
2026-08-19 13:50:10 +0200

    MMA-8488 Oppdatert README, la til LICENSE
    * Ryddet testdata
    * Fjernet elastic-apm referanse
    * Fjernet vault referanser

--- arkiverdokumentproduksjon/src/test/java/no/nav/dokarkiv/arkiverdokumentproduksjon/tjoark112/OpprettJournalpostArkiverDokumenterDataUtil.java
+++ arkiverdokumentproduksjon/src/test/java/no/nav/dokarkiv/arkiverdokumentproduksjon/tjoark112/OpprettJournalpostArkiverDokumenterDataUtil.java
@@ -28,8 +28,6 @@ import static no.nav.dokarkiv.core.CoreConfig.ZONEID_NORGE;
 /**
  * Util for creating Journalpost for the OpprettJournalpostArkiverDokumenter
  * operation
- *
- * @author Stig Strøm
  */
 public class OpprettJournalpostArkiverDokumenterDataUtil {
 
@@ -48,7 +46,7 @@ public class OpprettJournalpostArkiverDokumenterDataUtil {
 	protected static final String DOKUMENT_INNHOLD = "ustrukturertInnhold";
 	protected static final String DOKUMENT_INNHOLD_BASE64 = Base64.getEncoder().encodeToString(DOKUMENT_INNHOLD.getBytes());//"dXN0cnVrdHVyZXJ0SW5uaG9sZA==";
 	protected static final String VARIANTFORMAT = "ARKIV";
-	protected static final String PERSONIDENT = "22027838743";
+	protected static final String PERSONIDENT = "17434424798";
 	protected static final String EKSTERNPART_NAVN = "Jippi Hurra";
 	protected static final String FAGSYSTEMKODE = FagsystemCode.FS22.name();
 	protected static final Long SAKSID = 312L;
